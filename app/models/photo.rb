347ca22aaffa5366e2c280570f695a60a014afd8
Guillermo Esteves <g@gesteves.com>
2015-04-19 14:46:56 -0400

    Put S3 settings in right place

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,10 +1,9 @@
 class Photo < ActiveRecord::Base
   belongs_to :entry, touch: true
   acts_as_list scope: :entry
-  has_attached_file :image
-  validates_attachment_content_type :image,
-    content_type: /image\/jpe?g/,
+  has_attached_file :image,
     storage: :s3,
     s3_credentials: "#{Rails.root}/config/s3.yml",
     url: ':s3_domain_url'
+  validates_attachment_content_type :image, content_type: /image\/jpe?g/
 end
