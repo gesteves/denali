acb4a0d745b309dcefe03aa110c247f55a0e4e2b
Guillermo Esteves <g@gesteves.com>
2015-04-19 15:07:32 -0400

    Turn off asset generation

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module Denali
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.generators.stylesheets = false
+    config.generators.javascripts = false
   end
 end
