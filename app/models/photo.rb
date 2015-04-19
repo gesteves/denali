e39bcb48b12907dfa0e306154baf8c9d8f8489bb
Guillermo Esteves <g@gesteves.com>
2015-04-19 11:48:52 -0400

    Set up S3 storage for paperclip

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,5 +2,9 @@ class Photo < ActiveRecord::Base
   belongs_to :entry, touch: true
   acts_as_list scope: :entry
   has_attached_file :image
-  validates_attachment_content_type :image, :content_type => /image\/jpe?g/
+  validates_attachment_content_type :image,
+    content_type: /image\/jpe?g/,
+    storage: :s3,
+    s3_credentials: "#{Rails.root}/config/s3.yml",
+    url: ':s3_domain_url'
 end
