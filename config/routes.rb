725bb100a4291e04cfeafc4fb07cc4d5c3ddb297
Guillermo Esteves <g@gesteves.com>
2015-04-20 19:52:04 -0400

    Set up routes for admin entries

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,16 @@ Rails.application.routes.draw do
   namespace :admin do
     get 'settings' => 'blogs#edit'
     patch 'settings/update' => 'blogs#update'
+
+    resources :entries, only: [:index, :new, :create, :edit, :update, :destroy] do
+      member do
+        get 'preview'
+      end
+      collection do
+        get 'queued'
+        get 'drafts'
+      end
+    end
   end
 
   get '/auth/:provider/callback' => 'sessions#create'
