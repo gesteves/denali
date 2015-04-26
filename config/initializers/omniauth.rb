2e91c5da4fa85247a508cd06ab0146d56c772ffd
Guillermo Esteves <guillermo@voxmedia.com>
2015-04-26 13:38:01 -0400

    Reorganize Gemfile a little bit

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
+  provider :google_oauth2, Rails.application.secrets.google_client_id, Rails.application.secrets.google_client_secret, {
     :scope => 'userinfo.email, userinfo.profile'
   }
 end
