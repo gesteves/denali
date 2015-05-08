e063bf6f135b491ff660ead58dbad3e1c557ee2d
Guillermo Esteves <g@gesteves.com>
2015-05-08 10:29:25 -0400

    Show published count

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,12 +1,17 @@
 module AdminHelper
 
+  def published_count(opts = {})
+    entries = Entry.published.count
+    entries > 0 ? content_tag(:span, "&middot; #{number_with_delimiter entries}".html_safe, opts) : ''
+  end
+
   def drafts_count(opts = {})
-    drafts = Entry.drafted.count
-    drafts > 0 ? content_tag(:span, "&middot; #{drafts}".html_safe, opts) : ''
+    entries = Entry.drafted.count
+    entries > 0 ? content_tag(:span, "&middot; #{number_with_delimiter entries}".html_safe, opts) : ''
   end
 
   def queued_count(opts = {})
-    queued = Entry.queued.count
-    queued > 0 ? content_tag(:span, "&middot; #{queued}".html_safe, opts) : ''
+    entries = Entry.queued.count
+    entries > 0 ? content_tag(:span, "&middot; #{number_with_delimiter entries}".html_safe, opts) : ''
   end
 end
