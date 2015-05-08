d714492fb030b42bb17a2fdafe9818b352c3d997
Guillermo Esteves <g@gesteves.com>
2015-05-08 10:27:00 -0400

    Show queue and draft count

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,2 +1,12 @@
 module AdminHelper
+
+  def drafts_count(opts = {})
+    drafts = Entry.drafted.count
+    drafts > 0 ? content_tag(:span, "&middot; #{drafts}".html_safe, opts) : ''
+  end
+
+  def queued_count(opts = {})
+    queued = Entry.queued.count
+    queued > 0 ? content_tag(:span, "&middot; #{queued}".html_safe, opts) : ''
+  end
 end
