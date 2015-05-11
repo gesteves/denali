eef7a8c6ff279648b8f4c808df3a74b3d337b881
Guillermo Esteves <g@gesteves.com>
2015-05-11 08:56:48 -0400

    Reject blank photos

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -14,7 +14,7 @@ class Entry < ActiveRecord::Base
   before_save :set_published_date, if: :is_published?
   before_save :set_entry_slug
 
-  accepts_nested_attributes_for :photos, allow_destroy: true, reject_if: lambda { |attributes| attributes['source_file'].blank? && attributes['source_url'].blank? }
+  accepts_nested_attributes_for :photos, allow_destroy: true, reject_if: lambda { |attributes| attributes['source_file'].blank? && attributes['source_url'].blank? && attributes['id'].blank? }
 
   def is_photo?
     self.photos_count > 0
