cd7b7d0f9c2d2425e88ac27fa21b73eb05bf286c
Guillermo Esteves <g@gesteves.com>
2015-05-25 14:24:24 -0400

    Show full published date

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,4 +39,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.time_zone = 'Eastern Time (US & Canada)'
 end
