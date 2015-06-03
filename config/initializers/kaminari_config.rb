d7abefd3b51c817a5d579cff5f7f062fe7c173f2
Guillermo Esteves <g@gesteves.com>
2015-06-03 19:13:01 -0400

    Tweak pagination

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -1,7 +1,7 @@
 Kaminari.configure do |config|
   config.default_per_page = 10
   config.max_per_page = nil
-  config.window = 2
+  config.window = 4
   config.outer_window = 0
   config.left = 0
   config.right = 0
