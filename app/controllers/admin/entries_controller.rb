b118594868a15a40ae2b3a1f9dc6b21bcb254cae
Guillermo Esteves <g@gesteves.com>
2015-06-08 18:43:27 -0400

    Redirect to entry after created and published

--- app/controllers/admin/entries_controller.rb
+++ app/controllers/admin/entries_controller.rb
@@ -143,7 +143,7 @@ class Admin::EntriesController < AdminController
 
     def get_redirect_url(entry)
       if entry.is_published?
-        admin_entries_path
+        permalink(entry, { path_only: false })
       elsif entry.is_queued?
         queued_admin_entries_path
       else
