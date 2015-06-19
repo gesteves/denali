c87b7d2af7ad265c305a5a3ab9d030dd70d415e6
Guillermo Esteves <g@gesteves.com>
2015-06-18 20:13:35 -0400

    Set up image sizes in config file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,32 +21,8 @@ module ApplicationHelper
     content_tag :img, nil, html_options
   end
 
-  def photo_sizes
-    sizes = []
-    sizes << [320, 480]          # iPhone
-    sizes << [320 * 2, 480 * 2]  # iPhone 4
-    sizes << [320 * 2, 568 * 2]  # iPhone 5
-    sizes << [375 * 2, 667 * 2]  # iPhone 6
-    sizes << [414 * 3, 736 * 3]  # iPhone 6 plus
-    sizes << [1024, 768]         # iPad
-    sizes << [1024 * 2, 768 * 2] # iPad Air
-    sizes << [1280, 1280 * 2]    # Macbook
-    sizes << [1440, 1440 * 2]    # Macbook
-    sizes << [1920, 1920 * 2]    # iMac
-    sizes.flatten.sort.uniq
-  end
-
-  def photo_thumb_sizes
-    sizes = []
-    sizes << [160, 240]          # iPhone
-    sizes << [160 * 2, 240 * 2]  # iPhone 4
-    sizes << [160 * 2, 284 * 2]  # iPhone 5
-    sizes << [188 * 2, 223 * 2]  # iPhone 6
-    sizes << [207 * 3, 246 * 3]  # iPhone 6 plus
-    sizes << [342, 258]          # iPad
-    sizes << [342 * 2, 258 * 2]  # iPad Air
-    sizes << [320, 320 * 2]      # Macbook
-    sizes.flatten.sort.uniq
+  def get_srcset(photo_type)
+    PHOTO_SIZES[photo_type].sort.uniq
   end
 
   def copyright_years
---
+++ config/initializers/photo_sizes.rb
@@ -0,0 +1,1 @@
+PHOTO_SIZES = YAML.load_file(Rails.root.join('config/photo_sizes.yml'))
