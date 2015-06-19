09678b2e0e266006682e35b6e66ee4f959fe7665
Guillermo Esteves <g@gesteves.com>
2015-06-18 21:15:34 -0400

    Refactor admin helper

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,17 +1,19 @@
 module AdminHelper
 
   def published_count(opts = {})
-    entries = Entry.published.count
-    entries > 0 ? content_tag(:span, "&middot; #{number_with_delimiter entries}".html_safe, opts) : ''
+    render_count Entry.published.count, opts
   end
 
   def drafts_count(opts = {})
-    entries = Entry.drafted.count
-    entries > 0 ? content_tag(:span, "&middot; #{number_with_delimiter entries}".html_safe, opts) : ''
+    render_count Entry.drafted.count, opts
   end
 
   def queued_count(opts = {})
-    entries = Entry.queued.count
+    render_count Entry.queued.count, opts
+  end
+
+  private
+  def render_count(entries, opts = {})
     entries > 0 ? content_tag(:span, "&middot; #{number_with_delimiter entries}".html_safe, opts) : ''
   end
 end
