214bcc2891745f77f4812c8627ef6fc960be88b2
Guillermo Esteves <g@gesteves.com>
2015-06-20 10:44:35 -0400

    Simplify this

--- app/jobs/twitter_job.rb
+++ app/jobs/twitter_job.rb
@@ -12,27 +12,17 @@ class TwitterJob < ActiveJob::Base
       config.access_token        = ENV['twitter_access_token']
       config.access_token_secret = ENV['twitter_access_token_secret']
     end
-    config = twitter.configuration
-    tweet = build_tweet(entry, config)
-
+    tweet = build_tweet(entry, twitter.configuration.short_url_length_https, twitter.configuration.characters_reserved_per_media)
     if entry.is_photo?
-      twitter.update_with_media(tweet, get_photo(entry, config))
+      width = twitter.configuration.photo_sizes[:large].width
+      twitter.update_with_media(tweet, open(entry.photos.first.url(width)))
     else
       twitter.update(tweet)
     end
   end
 
-  def build_tweet(entry, config)
-    max_length = max_length(entry, config)
-    "#{truncate(entry.formatted_title, length: max_length, omission: '…')} #{permalink_url(entry)}"
-  end
-
-  def max_length(entry, config)
-    entry.is_text? ? 135 - config.short_url_length_https : 135 - config.short_url_length_https - config.characters_reserved_per_media
-  end
-
-  def get_photo(entry, config)
-    width = config.photo_sizes[:large].width || 2048
-    open(entry.photos.first.url(width))
+  def build_tweet(entry, short_url_length, media_length)
+    max_length = entry.is_text? ? 137 - short_url_length : 137 - short_url_length - media_length
+    "#{truncate(entry.formatted_title, length: max_length, omission: '… ')}#{permalink_url(entry)}"
   end
 end
