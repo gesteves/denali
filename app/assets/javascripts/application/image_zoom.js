ecf77669e6080b7464678f7aebe6e67af23e9dd5
Guillermo Esteves <g@gesteves.com>
2015-07-13 19:35:54 -0400

    Better way of zooming images

--- app/assets/javascripts/application/image_zoom.js
+++ app/assets/javascripts/application/image_zoom.js
@@ -6,17 +6,26 @@ Denali.ImageZoom = (function () {
   var opts = {
     images          : '.entry__image',
     zoomable_class  : 'entry__image--zoomable',
-    zoom_class      : 'entry__image--fit'
+    zoom_class      : 'entry__image--fit',
+    max_width       : 1920
   };
 
   var zoomable_images = [];
   var init = function () {
     var images = document.querySelectorAll(opts.images);
     var image,
+        original_width,
+        original_height,
+        image_ratio,
+        max_width,
         height;
     for (var i = 0; i < images.length; i++) {
       image = images[i];
-      height = parseInt(image.getAttribute('data-height-original'));
+      original_height = parseInt(image.getAttribute('data-height-original'));
+      original_width = parseInt(image.getAttribute('data-width-original'));
+      image_ratio = original_height/original_width;
+      max_width = Math.min(original_width, window.innerWidth, opts.max_width);
+      height = max_width * image_ratio;
       if (height > window.innerHeight) {
         image.classList.add(opts.zoomable_class);
         image.addEventListener('click', toggleZoom);
