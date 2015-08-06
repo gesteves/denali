728569b5140725c903e3053cd2d1d9d999a034f5
Guillermo Esteves <guillermo@voxmedia.com>
2015-08-06 08:48:09 -0400

    Update yo job

--- app/jobs/yo_job.rb
+++ app/jobs/yo_job.rb
@@ -1,9 +1,12 @@
-require 'net/http'
 class YoJob < EntryJob
 
   queue_as :default
 
   def perform(entry)
-    Net::HTTP.post_form(URI.parse('https://api.justyo.co/yoall/'), { api_token: ENV['yo_api_token'], link: permalink_url(entry) })
+    body = {
+      api_token: ENV['yo_api_token'],
+      link: permalink_url(entry)
+    }
+    HTTParty.post('https://api.justyo.co/yoall/', body: body)
   end
 end
