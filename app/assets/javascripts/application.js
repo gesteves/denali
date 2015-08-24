3fae913eef5863e2f0a5089c8fea8ad80bb4c99c
Guillermo Esteves <guillermo@voxmedia.com>
2015-08-24 09:49:54 -0400

    Switch order

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,14 +5,6 @@
 Turbolinks.enableProgressBar();
 Denali.Analytics.init();
 
-// Attach event listeners
-document.addEventListener('page:change', picturefill);
-document.addEventListener('page:change', Denali.SocialShare.init);
-document.addEventListener('page:change', Denali.Shortcuts.init);
-document.addEventListener('page:change', Denali.ImageZoom.init);
-document.addEventListener('orientationchange', Denali.ImageZoom.init);
-document.addEventListener('keydown', Denali.Shortcuts.handleKeyPress);
-
 // I'm loading scripts async, so if the page has finished loading then
 // I need to init these scripts directly, because the `page:change`
 // event has already fired.
@@ -21,3 +13,11 @@ if (document.readyState !== 'loading') {
   Denali.Shortcuts.init();
   Denali.ImageZoom.init();
 }
+
+// Attach event listeners
+document.addEventListener('page:change', picturefill);
+document.addEventListener('page:change', Denali.SocialShare.init);
+document.addEventListener('page:change', Denali.Shortcuts.init);
+document.addEventListener('page:change', Denali.ImageZoom.init);
+document.addEventListener('orientationchange', Denali.ImageZoom.init);
+document.addEventListener('keydown', Denali.Shortcuts.handleKeyPress);
