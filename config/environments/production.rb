b1bad077c644d424be1366db6df6c9a3b491917c
Guillermo Esteves <guillermo@voxmedia.com>
2015-08-26 09:45:26 -0400

    Set asset host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   end
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
+  config.action_controller.asset_host = "www.allencompassingtrip.com"
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
