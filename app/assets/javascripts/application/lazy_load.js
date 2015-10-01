17eeea919dacbaf1a882b6c149d864e458b24271
Guillermo Esteves <g@gesteves.com>
2015-10-01 10:28:45 -0400

    Simplify lazy load js

--- app/assets/javascripts/application/lazy_load.js
+++ app/assets/javascripts/application/lazy_load.js
@@ -5,7 +5,8 @@ Denali.LazyLoad = (function () {
 
   var opts = {
     images     : '.js-lazy-load',
-    load_class : 'js-lazy-load'
+    load_class : 'js-lazy-load',
+    offset     : 500
   };
 
   var requested_animation_frame = false;
@@ -22,8 +23,6 @@ Denali.LazyLoad = (function () {
     var image,
         images,
         image_top,
-        image_bottom,
-        top,
         bottom;
 
     images = document.querySelectorAll(opts.images);
@@ -32,14 +31,12 @@ Denali.LazyLoad = (function () {
       return;
     }
 
-    top = window.scrollY;
-    bottom = top + window.innerHeight;
+    bottom = window.scrollY + window.innerHeight;
 
     for (var i = 0; i < images.length; i++) {
       image = images[i];
       image_top = image.getBoundingClientRect().top;
-      image_bottom = image.getBoundingClientRect().bottom;
-      if ((image_top <= bottom) && (image_bottom >= top)) {
+      if (image_top <= bottom + opts.offset) {
         loadImage(image);
       }
     }
