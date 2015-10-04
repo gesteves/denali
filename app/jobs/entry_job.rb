16a05128ecda31b0c58a2da3d824e467fb549683
Guillermo Esteves <guillermo@voxmedia.com>
2015-10-04 13:54:35 -0400

    Use correct protocol in scheduled jobs

--- app/jobs/entry_job.rb
+++ app/jobs/entry_job.rb
@@ -3,7 +3,8 @@ class EntryJob < ActiveJob::Base
 
   def permalink_url(entry)
     year, month, day, id, slug = entry.slug_params
-    Rails.application.routes.url_helpers.entry_long_url(year, month, day, id, slug, { host: entry.blog.domain })
+    protocol = Rails.configuration.force_ssl ? 'https' : 'http'
+    Rails.application.routes.url_helpers.entry_long_url(year, month, day, id, slug, { host: entry.blog.domain, protocol: protocol })
   end
 
   def permalink_path(entry)
