d6b858eb06463d51ff08b98d84adc78e966f1016
Guillermo Esteves <g@gesteves.com>
2015-11-14 17:13:38 -0500

    Break up 500px job

--- app/jobs/five_hundred_job.rb
+++ app/jobs/five_hundred_job.rb
@@ -7,30 +7,32 @@ class FiveHundredJob < EntryJob
     access_token = OAuth::AccessToken.new(consumer, ENV['fivehundredpx_access_token'], ENV['fivehundredpx_access_token_secret'])
 
     opts = {
-        name: entry.formatted_title,
-        tags: entry.tag_list.join(','),
-        privacy: 0
-      }
+      name: entry.formatted_title,
+      tags: entry.tag_list.join(','),
+      privacy: 0
+    }
 
     if entry.body.present?
-        opts[:description] = "#{entry.plain_body}\n\n#{permalink_url(entry)}"
-      else
-        opts[:description] = permalink_url(entry)
-      end
+      opts[:description] = "#{entry.plain_body}\n\n#{permalink_url(entry)}"
+    else
+      opts[:description] = permalink_url(entry)
+    end
 
     entry.photos.each do |p|
-        response = access_token.post('https://api.500px.com/v1/photos', opts)
-        if response.code == '200'
-          body = JSON.parse(response.body)
-          opts = {
-            upload_key: body['upload_key'],
-            photo_id: body['photo']['id'],
-            file: File.new(open(p.original_url).path),
-            consumer_key: ENV['fivehundredpx_consumer_key'],
-            access_key: ENV['fivehundredpx_access_token']
-          }
-          HTTMultiParty.post('http://upload.500px.com/v1/upload', body: opts)
-        end
+      response = access_token.post('https://api.500px.com/v1/photos', opts)
+      upload_photo(p, response.body) if response.code == '200'
     end
+  end
+
+  def upload_photo(photo, response_body)
+    body = JSON.parse(response_body)
+    opts = {
+      upload_key: body['upload_key'],
+      photo_id: body['photo']['id'],
+      file: File.new(open(photo.original_url).path),
+      consumer_key: ENV['fivehundredpx_consumer_key'],
+      access_key: ENV['fivehundredpx_access_token']
+    }
+    HTTMultiParty.post('http://upload.500px.com/v1/upload', body: opts)
   end
 end
