4470ff4c531170db717aed5cb58cbc585b4f08aa
Guillermo Esteves <g@gesteves.com>
2015-11-21 19:01:33 -0500

    Show the date queued entries publish

--- app/helpers/admin/entries_helper.rb
+++ app/helpers/admin/entries_helper.rb
@@ -1,2 +1,10 @@
 module Admin::EntriesHelper
+  def publish_date_for_queued(entry, counter)
+    days = if Time.now.utc.hour < 17
+      counter
+    else
+      counter + 1
+    end
+    (Time.now + days.days).strftime('%A, %B %-d, %Y')
+  end
 end
