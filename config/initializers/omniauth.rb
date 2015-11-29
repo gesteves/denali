b0f82c5b6ba6f0ce97ba474af646e592c387d8c1
Guillermo Esteves <guillermo@voxmedia.com>
2015-11-29 10:33:34 -0500

    Skip jwt encoding

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,7 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['google_client_id'], ENV['google_client_secret'], {
-    :scope => 'userinfo.email, userinfo.profile'
+    scope: 'userinfo.email, userinfo.profile',
+    skip_jwt: true
   }
 end
