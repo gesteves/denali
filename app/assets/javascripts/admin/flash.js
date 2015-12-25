12e086a11e4852a027a7b455b11ef561c25f148e
Guillermo Esteves <guillermo@voxmedia.com>
2015-12-25 14:21:54 -0500

    Clear flashes automatically

--- app/assets/javascripts/admin/flash.js
+++ app/assets/javascripts/admin/flash.js
@@ -5,18 +5,24 @@ Denali.Flash = (function ($) {
 
   var opts = {
     flash_selector  : '.flash',
-    close_selector  : '.js-flash-close'
+    close_selector  : '.js-flash-close',
+    timeout_length  : 5000
   };
 
-  var flash;
+  var flash,
+      timeout_id;
 
   var init = function () {
     flash = $(opts.flash_selector);
     flash.on('click', opts.close_selector, closeFlash);
+    timeout_id = setTimeout(function () {
+      flash.fadeOut();
+    }, opts.timeout_length);
   };
 
   var closeFlash = function () {
     flash.fadeOut();
+    clearTimeout(timeout_id);
     return false;
   };
 
