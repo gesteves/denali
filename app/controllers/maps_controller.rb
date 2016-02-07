2536738ee22e2d0a047a9c827bc7a6ccea7494fa
Guillermo Esteves <guillermo@voxmedia.com>
2016-02-06 20:33:26 -0500

    Cache json for 24 hours

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -5,7 +5,7 @@ class MapsController < ApplicationController
 
   def photos
     @entries = @photoblog.entries.photo_entries.includes(:photos).published
-    expires_in 60.minutes, :public => true
+    expires_in 24.hours, :public => true
     respond_to do |format|
       format.json
     end
