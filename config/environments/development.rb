cb76949a1d6218ed6bbd7194f791bb0ae07fd719
Guillermo Esteves <g@gesteves.com>
2016-02-11 21:35:43 -0500

    Consider dev requests local

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
   config.eager_load = false
 
   # Show full error reports and disable caching.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
