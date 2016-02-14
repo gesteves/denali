abf76ba74bf59a9ecdad6f7a270675581a6052d5
Guillermo Esteves <guillermo@voxmedia.com>
2016-02-13 20:46:58 -0500

    Show 100 webhooks per page

--- app/controllers/admin/slack_incoming_webhooks_controller.rb
+++ app/controllers/admin/slack_incoming_webhooks_controller.rb
@@ -1,7 +1,7 @@
 class Admin::SlackIncomingWebhooksController < AdminController
   def index
     @page = params[:page] || 1
-    @webhooks = @photoblog.slack_incoming_webhooks.order('created_at DESC').page(@page)
+    @webhooks = @photoblog.slack_incoming_webhooks.order('created_at DESC').page(@page).per(100)
     @page_title = 'Slack Incoming Webhooks'
   end
 
