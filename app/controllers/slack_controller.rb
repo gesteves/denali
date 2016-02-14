f778d694ccccef0d3a75aae9d5515c252072c6fa
Guillermo Esteves <guillermo@voxmedia.com>
2016-02-13 20:33:52 -0500

    Add back the state check

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -2,13 +2,17 @@ class SlackController < ApplicationController
   before_action :domain_redirect
 
   def index
-    expires_in 0, private: true, must_revalidate: true
-    if params[:code].present?
+    expires_in 0, private: true, must_revalidate: true 
+    if params[:code].present? && params[:state] == session[:slack_state]
+      session[:slack_state] = nil
       token = get_access_token(params[:code])
       if token['ok']
         @webhook = SlackIncomingWebhook.new(team_name: token['team_name'], team_id: token['team_id'], channel: token['incoming_webhook']['channel'], url: token['incoming_webhook']['url'], configuration_url: token['incoming_webhook']['configuration_url'], blog_id: @photoblog.id)
         @added = @webhook.save
       end
+    else
+      redirect_to slack_path if params[:state].present?
+      @state = session[:slack_state] = SecureRandom.hex(10)
     end
   end
 
