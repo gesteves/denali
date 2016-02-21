acb64de4a817cd5abd203976760b14c2cc739fe4
Guillermo Esteves <g@gesteves.com>
2016-02-21 12:14:52 -0500

    Test photo caption formatting

--- test/models/photo_test.rb
+++ test/models/photo_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class PhotoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'photo formatting should work' do
+    photo = Photo.new(caption: 'This is the *caption* you\'re looking for.')
+    photo.save
+    assert_equal photo.plain_caption, 'This is the caption you’re looking for.'
+    assert_equal photo.formatted_caption, "<p>This is the <em>caption</em> you&rsquo;re looking for.</p>\n"
+  end
 end
