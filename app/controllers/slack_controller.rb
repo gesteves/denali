deeb22425c187596ba9788b0a0ee27cf6311819b
Guillermo Esteves <g@gesteves.com>
2016-02-21 10:42:56 -0500

    More clean up of Slack controller

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -7,23 +7,25 @@ class SlackController < ApplicationController
       @webhook = save_webhook(params[:code])
       render 'success'
     else
-      redirect_to slack_path if params[:state].present?
       @state = session[:slack_state] = SecureRandom.hex(10)
+      @client_id = ENV['slack_client_id']
     end
   end
 
   private
 
   def save_webhook(code)
-    response = HTTParty.get("https://slack.com/api/oauth.access?code=#{code}&client_id=#{ENV['slack_client_id']}&client_secret=#{ENV['slack_client_secret']}&redirect_uri=#{slack_url}")
-    token = JSON.parse(response.body)
+    token = get_access_token(code)
     if token['ok']
       webhook = SlackIncomingWebhook.new(team_name: token['team_name'], team_id: token['team_id'], channel: token['incoming_webhook']['channel'], url: token['incoming_webhook']['url'], configuration_url: token['incoming_webhook']['configuration_url'], blog_id: @photoblog.id)
       webhook.save
       logger.info "[INFO] Slack incoming webhook created for the #{webhook.team_name} team in the #{webhook.channel} channel."
-    else
-      webhook = nil
     end
     webhook
+  end
+
+  def get_access_token(code)
+    response = HTTParty.get("https://slack.com/api/oauth.access?code=#{code}&client_id=#{ENV['slack_client_id']}&client_secret=#{ENV['slack_client_secret']}&redirect_uri=#{slack_url}")
+    JSON.parse(response.body)
   end
 end
