c8c71b266c850523d80e244f9b34930bc81a32f5
Guillermo Esteves <g@gesteves.com>
2016-02-21 13:16:51 -0500

    Add photo test

--- test/models/photo_test.rb
+++ test/models/photo_test.rb
@@ -7,4 +7,11 @@ class PhotoTest < ActiveSupport::TestCase
     assert_equal photo.plain_caption, 'This is the caption you’re looking for.'
     assert_equal photo.formatted_caption, "<p>This is the <em>caption</em> you&rsquo;re looking for.</p>\n"
   end
+
+  test 'photo dimensions' do
+    photo = photos(:peppers)
+    assert photo.is_vertical?
+    assert_not photo.is_square?
+    assert_not photo.is_horizontal?
+  end
 end
