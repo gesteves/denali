94c6a3b783eafbfa5ea6911458ad10b729f5532b
Guillermo Esteves <g@gesteves.com>
2016-02-21 14:10:24 -0500

    Add admin controller test

--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class AdminControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should redirect to sign in page if not signed in' do
+    get :index
+    assert_redirected_to signin_path
+  end
+
+  test 'should redirect to admin entries list' do
+    session[:user_id] = users(:guille).id
+    get :index
+    assert_redirected_to admin_entries_path
+  end
 end
