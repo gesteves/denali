fec8ef7e2ac53b143147a13a4b09640e4dddaa4a
Guillermo Esteves <g@gesteves.com>
2016-02-21 10:23:09 -0500

    Clean up Slack controller

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -4,12 +4,8 @@ class SlackController < ApplicationController
     expires_in 0, private: true, must_revalidate: true
     if params[:code].present? && params[:state] == session[:slack_state]
       session[:slack_state] = nil
-      token = get_access_token(params[:code])
-      if token['ok']
-        @webhook = SlackIncomingWebhook.new(team_name: token['team_name'], team_id: token['team_id'], channel: token['incoming_webhook']['channel'], url: token['incoming_webhook']['url'], configuration_url: token['incoming_webhook']['configuration_url'], blog_id: @photoblog.id)
-        @added = @webhook.save
-        logger.info "[INFO] Slack incoming webhook created for the #{@webhook.team_name} team in the #{@webhook.channel} channel."
-      end
+      @webhook = save_webhook(params[:code])
+      render 'success'
     else
       redirect_to slack_path if params[:state].present?
       @state = session[:slack_state] = SecureRandom.hex(10)
@@ -18,8 +14,16 @@ class SlackController < ApplicationController
 
   private
 
-  def get_access_token(code)
+  def save_webhook(code)
     response = HTTParty.get("https://slack.com/api/oauth.access?code=#{code}&client_id=#{ENV['slack_client_id']}&client_secret=#{ENV['slack_client_secret']}&redirect_uri=#{slack_url}")
-    JSON.parse(response.body)
+    token = JSON.parse(response.body)
+    if token['ok']
+      webhook = SlackIncomingWebhook.new(team_name: token['team_name'], team_id: token['team_id'], channel: token['incoming_webhook']['channel'], url: token['incoming_webhook']['url'], configuration_url: token['incoming_webhook']['configuration_url'], blog_id: @photoblog.id)
+      webhook.save
+      logger.info "[INFO] Slack incoming webhook created for the #{webhook.team_name} team in the #{webhook.channel} channel."
+    else
+      webhook = nil
+    end
+    webhook
   end
 end
--- test/controllers/slack_controller_test.rb
+++ test/controllers/slack_controller_test.rb
@@ -3,7 +3,13 @@ require 'test_helper'
 class SlackControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
+    assert_template :index
     assert_response :success
+  end
+
+  test "should redirect to index index" do
+    get :index, state: 123
+    assert_redirected_to slack_path
   end
 
 end
