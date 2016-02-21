8ef9fb8771045b660f1615cb516c72fbd8a4950e
Guillermo Esteves <g@gesteves.com>
2016-02-21 13:37:19 -0500

    Test maps json

--- test/controllers/maps_controller_test.rb
+++ test/controllers/maps_controller_test.rb
@@ -6,4 +6,17 @@ class MapsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should generate json" do
+    get :photos, format: 'json'
+    assert_template :photos
+    assert_response :success
+  end
+
+  test "should only return mapped images" do
+    get :photos, format: 'json'
+    json = JSON.parse(@response.body)
+    assert_not_empty json.select { |e| e['properties']['title'] == entries(:peppers).title }
+    assert_empty json.select { |e| e['properties']['title'] == entries(:potomac).title }
+  end
+
 end
