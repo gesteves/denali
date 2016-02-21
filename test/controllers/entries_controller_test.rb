37bc2b7d8e660173b4dd823354c10842e69aa3f0
Guillermo Esteves <g@gesteves.com>
2016-02-21 12:30:15 -0500

    Add entry controller tests

--- test/controllers/entries_controller_test.rb
+++ test/controllers/entries_controller_test.rb
@@ -1,5 +1,27 @@
 require 'test_helper'
 
 class EntriesControllerTest < ActionController::TestCase
+  test 'entries index should render correctly' do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:entries)
+    assert_template :index
+    assert_template layout: 'layouts/application'
+    assert_select '.entry-list' do
+      assert_select '.entry-list__item', 2
+    end
+  end
 
+  test 'photo page should render correctly' do
+    entry = entries(:peppers)
+    get :show, year: entry.published_at.strftime('%Y'), month: entry.published_at.strftime('%-m'), day: entry.published_at.strftime('%-d'), id: entry.id, slug: entry.slug
+    assert_response :success
+    assert_not_nil assigns(:entry)
+    assert_template :show
+    assert_template layout: 'layouts/application'
+    assert_select '.entry', 1
+    assert_select '.entry__photo', 1
+    assert_select '.entry__image', 1
+    assert_select '.entry__headline', 1
+  end
 end
