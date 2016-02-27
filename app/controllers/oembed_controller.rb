0d465f8f10c70f7590a2c64c3b3e187bbf9960f4
Guillermo Esteves <g@gesteves.com>
2016-02-27 12:39:42 -0500

    Clean up oEmbed controller

--- app/controllers/oembed_controller.rb
+++ app/controllers/oembed_controller.rb
@@ -1,24 +1,29 @@
 class OembedController < ApplicationController
-  before_action :set_max_age
+  before_action :set_max_age, :load_entry, :set_request_format
 
   def show
+    @url, @width, @height = get_photo(@entry, 1200, params[:maxwidth], params[:maxheight])
+    @thumb_url, @thumb_width, @thumb_height = get_photo(@entry, 300, params[:maxwidth], params[:maxheight])
+    respond_to do |format|
+      format.json
+      format.xml
+    end
+  end
+
+  private
+
+  def set_request_format
+    request.format = params[:format] || 'json'
+  end
+
+  def load_entry
     url = Rails.application.routes.recognize_path(params[:url])
     if url[:controller] != 'entries' || url[:action] != 'show' || url[:id].nil?
       raise ActiveRecord::RecordNotFound
     else
       @entry = @photoblog.entries.includes(:photos, :user).published.find(url[:id])
-      @url, @width, @height = get_photo(@entry, 1200, params[:maxwidth], params[:maxheight])
-      @thumb_url, @thumb_width, @thumb_height = get_photo(@entry, 300, params[:maxwidth], params[:maxheight])
-
-      request.format = params[:format] || 'json'
-      respond_to do |format|
-        format.json
-        format.xml
-      end
     end
   end
-
-  private
 
   def get_photo(entry, default_width = 1200, maxwidth, maxheight)
     if entry.is_photo?
