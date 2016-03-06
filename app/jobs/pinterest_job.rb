bc174c6de6c81cf57c05bba85b24385195598a6b
Guillermo Esteves <g@gesteves.com>
2016-03-06 08:18:57 -0500

    Only run pinterest job in production

--- app/jobs/pinterest_job.rb
+++ app/jobs/pinterest_job.rb
@@ -8,9 +8,11 @@ class PinterestJob < ApplicationJob
       link: entry.permalink_url,
       image_url: entry.photos.first.original_url
     }
-    response = HTTParty.post('https://api.pinterest.com/v1/pins/', query: opts, headers: { 'Authorization' => "BEARER #{ENV['pinterest_token']}" }) if Rails.env.production?
-    if response.code >= 400
-      raise response.body
+    if Rails.env.production?
+      response = HTTParty.post('https://api.pinterest.com/v1/pins/', query: opts, headers: { 'Authorization' => "BEARER #{ENV['pinterest_token']}" })
+      if response.code >= 400
+        raise response.body
+      end
     end
   end
 end
