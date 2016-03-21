31ba36bc61ed8d062e633b9750aaa64b000b79ed
Guillermo Esteves <g@gesteves.com>
2016-03-20 21:12:19 -0400

    Remove photos controller

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,6 @@ Rails.application.routes.draw do
     patch 'settings/update'           => 'blogs#update'
     get '/slack(/page/:page)'         => 'slack_incoming_webhooks#index',  constraints: { page: /\d+/ }, :as => :slack
 
-    resources :photos, only: [:index, :update], concerns: :paginatable
-
     resources :entries, only: [:index, :new, :create, :edit, :update, :destroy], concerns: :paginatable do
       member do
         get 'preview'
