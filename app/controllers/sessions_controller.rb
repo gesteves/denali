776e2059e0ee2b69d0494ad2f1a2685f58a43358
Niv Shah <nivshah@gmail.com>
2016-03-29 16:24:35 -0500

    Single user accepted, user must put their email in config
    This might be pulled later, because committing email addresses
    is dumb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
     auth_hash = request.env['omniauth.auth']
     email = auth_hash.try(:[], 'info').try(:[], 'email')
 
-    if email =~ /@gesteves\.com$/
+    if email == Rails.application.config.site['auth_email']
       flash[:notice] = "Welcome, #{auth_hash['info']['name']}!"
       user = User.from_omniauth(env['omniauth.auth'])
       session[:user_id] = user.id
