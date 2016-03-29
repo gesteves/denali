f25896e6f560a09ddc53774631457f9e3cc52709
Niv Shah <nivshah@gmail.com>
2016-03-29 13:45:50 -0500

    Same rule for social hash has site hash

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -7,7 +7,7 @@ class EntriesController < ApplicationController
   before_action :set_max_age, only: [:index, :tagged, :show]
 
   def index
-    raise ActiveRecord::RecordNotFound if @entries.empty?
+    # raise ActiveRecord::RecordNotFound if @entries.empty?
     respond_to do |format|
       format.html
       format.json
