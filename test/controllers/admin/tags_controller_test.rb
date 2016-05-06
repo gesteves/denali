b4a28c7798fa6b697004aecaadac1da1e7d91312
Guillermo Esteves <g@gesteves.com>
2016-05-06 18:03:16 -0400

    Fix failing test

--- test/controllers/admin/tags_controller_test.rb
+++ test/controllers/admin/tags_controller_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class Admin::TagsControllerTest < ActionController::TestCase
+  def setup
+    session[:user_id] = users(:guille).id
+  end
+
   test "should get index" do
     get :index
     assert_response :success
