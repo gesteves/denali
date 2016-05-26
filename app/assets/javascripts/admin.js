dbc07886760467f3e7f03b924cd424280f85a139
Guillermo Esteves <g@gesteves.com>
2016-05-26 19:02:49 -0400

    Lazy load admin images

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -2,8 +2,9 @@
 //= require ./vendors/jquery
 //= require ./vendors/jquery_ui
 //= require ./vendors/awesomplete
-//= require_tree ./admin
 //= require jquery_ujs
+//= require_tree ./admin
+//= require ./application/lazy_load
 
 'use strict';
 
@@ -13,8 +14,10 @@ if (document.readyState !== 'loading') {
   Denali.Entries.init();
   Denali.Flash.init();
   Denali.Queue.init();
+  Denali.LazyLoad.init();
 }
 
 document.addEventListener('page:change', Denali.Entries.init);
 document.addEventListener('page:change', Denali.Flash.init);
 document.addEventListener('page:change', Denali.Queue.init);
+document.addEventListener('page:change', Denali.LazyLoad.init);
