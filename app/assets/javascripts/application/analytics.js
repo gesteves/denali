28d835373ec3344127d6a455369a8aa2745c4f65
Guillermo Esteves <guillermo@voxmedia.com>
2016-06-02 10:55:45 -0400

    Gosquared

--- app/assets/javascripts/application/analytics.js
+++ app/assets/javascripts/application/analytics.js
@@ -4,13 +4,8 @@ Denali.Analytics = (function () {
   'use strict';
 
   var sendPageview = function () {
-    if (typeof ga !== 'undefined') {
-      ga('send', {
-        hitType  : 'pageview',
-        location : window.location.href,
-        page     : window.location.pathname,
-        title    : document.title
-      });
+    if (typeof _gs !== 'undefined') {
+      _gs('track', window.location.href, document.title);
     }
   };
 
