d4c78f56008479f6e764b37b7eaba642b5a26c0c
Guillermo Esteves <g@gesteves.com>
2016-06-12 15:18:09 -0400

    Skip domain redirect

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,6 +2,7 @@ class AdminController < ApplicationController
   layout 'admin'
   before_action :require_login
   before_action :no_cache
+  skip_before_action :domain_redirect
 
   def index
     redirect_to admin_entries_path
