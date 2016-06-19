17e9aeb401db03e9ce06abe2f802bdcf71cbdbc4
Guillermo Esteves <g@gesteves.com>
2016-06-19 15:45:18 -0400

    Reorganize before filters

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,8 +1,8 @@
 class AdminController < ApplicationController
   layout 'admin'
-  before_action :require_login
-  before_action :no_cache
   before_action :block_cloudfront
+  before_action :no_cache
+  before_action :require_login
   skip_before_action :domain_redirect
 
   def index
