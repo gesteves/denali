688d3678a8f7c11c4e967d471e7b248b938526d0
Guillermo Esteves <guillermo@voxmedia.com>
2016-06-22 13:43:26 -0400

    Map caching tweak

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -6,7 +6,6 @@ class MapsController < ApplicationController
   end
 
   def photos
-    @entries = @photoblog.entries.photo_entries.published.mapped.joins(:photos).includes(:photos).where('photos.latitude is not null AND photos.longitude is not null')
     expires_in 24.hours, public: true
     respond_to do |format|
       format.json
