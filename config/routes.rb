fe18c7f06cfcf51a0f05ff0b574568082608b6e7
Guillermo Esteves <g@gesteves.com>
2016-07-01 19:36:40 -0400

    Upgrade app

--- config/routes.rb
+++ config/routes.rb
@@ -65,4 +65,5 @@ Rails.application.routes.draw do
   match '/404', to: 'errors#file_not_found', via: :all
   match '/422', to: 'errors#unprocessable', via: :all
   match '/500', to: 'errors#internal_server_error', via: :all
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
