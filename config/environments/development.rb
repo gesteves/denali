d135a3a93b009659eafa216c9c35057520d4cf00
Guillermo Esteves <g@gesteves.com>
2016-07-01 19:47:23 -0400

    Move misplaced line

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,5 +51,5 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-end
   config.time_zone = 'Eastern Time (US & Canada)'
+end
