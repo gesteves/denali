76800e802aa6834bf82bd418c9047d7bb92c00c4
Guillermo Esteves <g@gesteves.com>
2016-07-01 21:43:17 -0400

    Set log level to info

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :debug
+  config.log_level = :info
   config.lograge.enabled = true
 
   # Prepend all log lines with the following tags.
