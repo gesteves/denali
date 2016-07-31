4280bbf8957d562829259d3e9556436f70d8ef4d
Guillermo Esteves <g@gesteves.com>
2016-07-31 14:37:40 -0400

    Revert "Add requestbin job"
    This reverts commit 87613c414c9971afde19d635bb917bfe1cea7470.

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: bundle exec passenger start -p $PORT --max-pool-size 3
-worker: bundle exec rake resque:work QUEUE=*
--- app/jobs/requestbin_job.rb
+++
@@ -1,8 +0,0 @@
-class RequestbinJob < ApplicationJob
-  queue_as :default
-
-  def perform(entry)
-    HTTParty.post('http://requestb.in/s3dg3us3', body: { url: entry.permalink_url, time: Time.now.to_s, env: Rails.env })
-  end
-
-end
