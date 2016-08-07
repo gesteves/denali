9a22621ff952844007c369b4076a0b6cc3964d19
Guillermo Esteves <g@gesteves.com>
2016-08-07 09:56:50 -0400

    Compress inline js

--- config/initializers/middleware.rb
+++ config/initializers/middleware.rb
@@ -1,4 +1,8 @@
+uglifier = Uglifier.new output: { comments: :none }
+
 Rails.application.config.middleware.use Rack::Deflater
 Rails.application.config.middleware.use HtmlCompressor::Rack, {
-  remove_javascript_protocol: false
+  javascript_compressor: uglifier,
+  remove_javascript_protocol: false,
+  compress_javascript: true,
 }
