e36983098d3201eb88c8ef6099764d3a28b48e06
Guillermo Esteves <g@gesteves.com>
2016-08-07 21:50:55 -0400

    Don’t use uglifier on inline js

--- config/initializers/middleware.rb
+++ config/initializers/middleware.rb
@@ -1,8 +1,4 @@
-uglifier = Uglifier.new output: { comments: :none }
-
 Rails.application.config.middleware.use Rack::Deflater
 Rails.application.config.middleware.use HtmlCompressor::Rack, {
-  javascript_compressor: uglifier,
-  remove_javascript_protocol: false,
-  compress_javascript: true,
+  remove_javascript_protocol: false
 }
