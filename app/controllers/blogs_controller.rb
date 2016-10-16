bf8edaf3a9deefc8573f12684cd178096fa32fbf
Guillermo Esteves <g@gesteves.com>
2016-10-16 13:33:55 -0400

    Update blogs_controller.rb

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,6 @@
 class BlogsController < ApplicationController
   before_action :check_if_user_has_visited
-  before_action :set_max_age, only: [:about]
+  before_action :set_entry_max_age
 
   def about
     fresh_when @photoblog, public: true
