9226c76cc17dc4c76d53e311f47a6784a5fef724
Guillermo Esteves <g@gesteves.com>
2016-11-27 16:14:28 -0500

    Try a different cropping method

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -41,7 +41,7 @@ class Photo < ApplicationRecord
     if opts[:square]
       opts[:h] = opts[:w]
       opts[:fit] = 'crop'
-      opts[:crop] = self.crop if self.crop.present?
+      opts[:crop] = 'faces,edges'
       opts.delete(:square)
     end
     Ix.path(self.original_path).to_url(opts.reject { |k,v| v.blank? })
