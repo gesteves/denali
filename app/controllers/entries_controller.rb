29da668016d225ebb409440819a2616a864480a6
Guillermo Esteves <g@gesteves.com>
2016-11-30 08:21:29 -0500

    Small refactor of entries controller

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -2,14 +2,16 @@ class EntriesController < ApplicationController
   include TagList
 
   before_action :set_request_format, only: [:index, :tagged, :show]
-  before_action :load_tags, :load_tagged_entries, only: [:tagged]
-  before_action :load_entries, only: [:index]
+  before_action :load_tags, only: [:tagged]
   before_action :set_max_age, only: [:index, :tagged]
   before_action :set_entry_max_age, only: [:show, :preview]
 
   def index
-    raise ActiveRecord::RecordNotFound if @entries.empty?
     if stale?(@photoblog, public: true)
+      @page = (params[:page] || 1).to_i
+      @count = (params[:count] || @photoblog.posts_per_page).to_i
+      @entries = @photoblog.entries.includes(:photos).published.page(@page).per(@count)
+      raise ActiveRecord::RecordNotFound if @entries.empty?
       respond_to do |format|
         format.html
         format.json
@@ -19,8 +21,11 @@ class EntriesController < ApplicationController
   end
 
   def tagged
-    raise ActiveRecord::RecordNotFound if @tags.empty? || @entries.empty?
     if stale?(@photoblog, public: true)
+      @page = (params[:page] || 1).to_i
+      @count = (params[:count] || @photoblog.posts_per_page).to_i
+      @entries = @photoblog.entries.includes(:photos).published.tagged_with(@tag_list, any: true).page(@page).per(@count)
+      raise ActiveRecord::RecordNotFound if @tags.empty? || @entries.empty?
       respond_to do |format|
         format.html
         format.json
@@ -30,8 +35,8 @@ class EntriesController < ApplicationController
   end
 
   def show
-    @entry = @photoblog.entries.includes(:photos, :user, :blog).published.find(params[:id])
     if stale?(@photoblog, public: true)
+      @entry = @photoblog.entries.includes(:photos, :user, :blog).published.find(params[:id])
       respond_to do |format|
         format.html {
           redirect_to(@entry.permalink_url, status: 301) unless params_match(@entry, params)
@@ -47,8 +52,8 @@ class EntriesController < ApplicationController
 
   def preview
     request.format = 'html'
-    @entry = @photoblog.entries.includes(:photos, :user, :blog).find(params[:id])
     if stale?(@photoblog, public: true)
+      @entry = @photoblog.entries.includes(:photos, :user, :blog).find(params[:id])
       respond_to do |format|
         format.html {
           if @entry.is_published?
@@ -75,9 +80,9 @@ class EntriesController < ApplicationController
   end
 
   def sitemap
-    @entries = @photoblog.entries.published
     expires_in 24.hours, public: true
     if stale?(@photoblog, public: true)
+      @entries = @photoblog.entries.published
       render format: 'xml'
     end
   end
@@ -94,18 +99,6 @@ class EntriesController < ApplicationController
     month == params[:month] &&
     day == params[:day] &&
     slug == params[:slug]
-  end
-
-  def load_entries
-    @page = (params[:page] || 1).to_i
-    @count = (params[:count] || @photoblog.posts_per_page).to_i
-    @entries = @photoblog.entries.includes(:photos).published.page(@page).per(@count)
-  end
-
-  def load_tagged_entries
-    @page = (params[:page] || 1).to_i
-    @count = (params[:count] || @photoblog.posts_per_page).to_i
-    @entries = @photoblog.entries.includes(:photos).published.tagged_with(@tag_list, any: true).page(@page).per(@count)
   end
 
   def set_request_format
