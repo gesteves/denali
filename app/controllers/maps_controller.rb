b63fa82a2f32aba66f804d567a3962c4a8e7686a
Guillermo Esteves <g@gesteves.com>
2016-12-17 13:08:43 -0500

    Return 404 if photo not found

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -18,6 +18,7 @@ class MapsController < ApplicationController
   def photo
     expires_in 24.hours, public: true
     @photo = Photo.joins(:entry).where(photos: { id: params[:id] }, entries: { status: 'published' }).limit(1).first
+    raise ActiveRecord::RecordNotFound if @photo.nil?
     if stale?(@photo, public: true)
       respond_to do |format|
         format.html { render layout: nil }
