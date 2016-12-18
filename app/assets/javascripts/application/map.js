b12b84e106118b85d1a395ed50bfa919479d6689
Guillermo Esteves <g@gesteves.com>
2016-12-17 23:28:55 -0500

    Refactor js

--- app/assets/javascripts/application/map.js
+++ app/assets/javascripts/application/map.js
@@ -50,66 +50,79 @@ Denali.Map = (function () {
     }
   };
 
+  var requestPopup = function (marker, photo_id) {
+    var request = new XMLHttpRequest();
+    request.open('GET', '/map/photo/' + photo_id + '.json', true);
+    request.onload = function() {
+      if (request.status >= 200 && request.status < 400) {
+        var response = JSON.parse(request.responseText);
+        marker.setPopupContent(response.html);
+        marker.off('popupopen');
+      }
+    };
+    request.send();
+  };
+
+  var setUpMarker = function (e) {
+    var marker = e.layer,
+        feature = marker.feature;
+    var photo_id = feature.properties.id;
+    marker.setIcon(L.divIcon({
+        className: 'map__marker map__marker--point',
+        html: '&bull;',
+        iconSize: [10, 10],
+        iconAnchor: [5, 5]
+      }));
+    this.bindPopup('', {
+      closeButton: true,
+      minWidth: 300
+    });
+    marker.on('popupopen', function () {
+      requestPopup(this, photo_id);
+    });
+  };
+
+  var setUpClusterIcon = function (cluster) {
+    return L.divIcon({
+        className: 'map__marker map__marker--cluster',
+        html: cluster.getChildCount(),
+        iconSize: cluster.getChildCount() > 99 ? [30, 30] : [20, 20],
+        iconAnchor: cluster.getChildCount() > 99 ? [15, 15] : [10, 10]
+      }
+    );
+  };
+
+  var setUpMarkerClusters = function (e) {
+    var cluster_group = new L.MarkerClusterGroup({
+      showCoverageOnHover: false,
+      maxClusterRadius: 30,
+      spiderfyDistanceMultiplier: 3,
+      iconCreateFunction: setUpClusterIcon
+    });
+
+    e.target.eachLayer(function (layer) {
+      cluster_group.addLayer(layer);
+    });
+
+    hash = new L.hash(map);
+    map.addLayer(cluster_group);
+    show_bar = false;
+    hideLoadingSpinner();
+  };
+
   var initMap = function () {
-    setTimeout(showLoadingSpinner, 500);
+    setTimeout(showLoadingSpinner, 100);
     var south_west = L.latLng(-90, -180),
         north_east = L.latLng(90, 180),
-        bounds = L.latLngBounds(south_west, north_east);
+        bounds = L.latLngBounds(south_west, north_east),
+        zoom = getZoom();
+        console.log(zoom);
     L.mapbox.accessToken = 'pk.eyJ1IjoiZ2VzdGV2ZXMiLCJhIjoiY2lrY3EyeDA3MG03Y3Y5a3V6d3MwNHR3cSJ9.qG9UBVJvti71fNvW5iKONA';
-    map = L.mapbox.map(opts.map_container_id, 'gesteves.ce0e3aae', { minZoom: getZoom(), maxZoom: 18, maxBounds: bounds });
+    map = L.mapbox.map(opts.map_container_id, 'gesteves.ce0e3aae', { minZoom: zoom, maxZoom: 18, maxBounds: bounds });
     var layer = L.mapbox.featureLayer();
 
-    layer.on('layeradd', function(e) {
-      var marker = e.layer,
-          feature = marker.feature;
-      var photo_id = feature.properties.id;
-      marker.setIcon(L.divIcon({
-          className: 'map__marker map__marker--point',
-          html: '&bull;',
-          iconSize: [10, 10],
-          iconAnchor: [5, 5]
-        }));
-      this.bindPopup('', {
-        closeButton: true,
-        minWidth: 300
-      });
-      marker.on('popupopen', function () {
-        var marker = this;
-        var request = new XMLHttpRequest();
-        request.open('GET', '/map/photo/' + photo_id + '.json', true);
-        request.onload = function() {
-          if (request.status >= 200 && request.status < 400) {
-            var response = JSON.parse(request.responseText);
-            marker.setPopupContent(response.html);
-            marker.off('popupopen');
-          }
-        };
-        request.send();
-      });
-    });
-    layer.loadURL('/map/photos.json').on('ready', function (e) {
-      var cluster_group = new L.MarkerClusterGroup({
-        showCoverageOnHover: false,
-        maxClusterRadius: 30,
-        spiderfyDistanceMultiplier: 3,
-        iconCreateFunction: function (cluster) {
-          return L.divIcon({
-              className: 'map__marker map__marker--cluster',
-              html: cluster.getChildCount(),
-              iconSize: cluster.getChildCount() > 99 ? [30, 30] : [20, 20],
-              iconAnchor: cluster.getChildCount() > 99 ? [15, 15] : [10, 10]
-            }
-          );
-        }
-      });
-      e.target.eachLayer(function (layer) {
-        cluster_group.addLayer(layer);
-      });
-      hash = new L.hash(map);
-      map.addLayer(cluster_group);
-      show_bar = false;
-      hideLoadingSpinner();
-    });
+    layer.on('layeradd', setUpMarker);
+    layer.loadURL('/map/photos.json').on('ready', setUpMarkerClusters);
   };
 
   return {
