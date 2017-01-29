c21fb0ae8b6255661343151909955e2004563856
Guillermo Esteves <g@gesteves.com>
2017-01-29 16:44:28 -0500

    Better errors

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,21 +1,30 @@
 class ErrorsController < ApplicationController
 
   def file_not_found
-    expires_in 24.hours, public: true
     @errors = [{ status: 404, message: 'Page not found' }]
-    render 'error', status: @errors.first[:status]
+    respond
   end
 
   def unprocessable
     @errors = [{ status: 422, message: 'Unprocessable entity' }]
-    render 'error', status: @errors.first[:status]
+    respond
   end
 
   def internal_server_error
     @status = 500
     @message = 'Internal server error'
     @errors = [{ status: 500, message: 'Internal server error' }]
-    render 'error', status: @errors.first[:status]
+    respond
+  end
+
+  private
+
+  def respond
+    respond_to do |format|
+      format.html { render 'error', status: @errors.first[:status] }
+      format.json { render 'error', status: @errors.first[:status] }
+      format.all { render plain: @errors.first[:message], status: @errors.first[:status] }
+    end
   end
 
 end
