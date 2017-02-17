a7cff74940d39bdd801a43b2879358dfeb477d50
Guillermo Esteves <g@gesteves.com>
2017-02-17 07:00:47 -0500

    Add timing label

--- app/assets/javascripts/application/speedindex.js
+++ app/assets/javascripts/application/speedindex.js
@@ -6,12 +6,16 @@ Denali.SpeedIndex = (function () {
   'use strict';
 
   var init = function () {
+    var body, label;
     if (typeof RUMSpeedIndex() !== 'undefined') {
+      body = document.querySelector('body');
+      label = body.getAttribute('data-context');
       ga('send', {
         hitType: 'timing',
         timingCategory: 'RUM',
         timingVar: 'speedindex',
-        timingValue: Math.round(RUMSpeedIndex())
+        timingValue: Math.round(RUMSpeedIndex()),
+        timingLabel: label
       });
     }
   };
