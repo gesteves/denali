9fae22980cdf2b28b7e7e3f8c1defd289b2f41b2
Guillermo Esteves <g@gesteves.com>
2017-03-11 19:22:45 -0500

    Only post to slack in production

--- app/jobs/slack_job.rb
+++ app/jobs/slack_job.rb
@@ -5,7 +5,7 @@ class SlackJob < ApplicationJob
     payload = { text: text }
     payload[:attachments] = [attachment] if attachment.present?
     payload[:channel] = channel if channel.present?
-    if !Rails.env.test? && ENV['slack_incoming_webhook'].present?
+    if Rails.env.production? && ENV['slack_incoming_webhook'].present?
       response = HTTParty.post(ENV['slack_incoming_webhook'], body: payload.to_json)
       if response.code >= 400
         raise response.body
