13fbb020b32a6a4fc35484f7c465e934f9820e32
Guillermo Esteves <g@gesteves.com>
2017-04-09 16:06:20 -0400

    Add a yml file for instagram tags

--- app/jobs/instagram_job.rb
+++ app/jobs/instagram_job.rb
@@ -2,24 +2,10 @@ class InstagramJob < BufferJob
   queue_as :default
 
   def perform(entry)
-    all_tags = entry.combined_tags.map { |t| t.slug.gsub(/-/, '') }
-
-    # Special tags for Fuji
-    if entry.equipment_list.include? 'fujifilm'
-      all_tags += %w{ fujifeed fujifilmx_us fujilove fujifilm_xseries }
-    end
-
-    if entry.tag_list.include? 'street photography'
-      alltags += %w{ spicollective ig_street everybodystreet }
-    end
-
-    # More tags
-    alltags += %w{ myfeatureshoot somewheremagazine }
-
     text_array = []
     text_array << entry.plain_title
     text_array << entry.plain_body if entry.body.present?
-    text_array << all_tags[0, 30].sort.map { |t| "##{t}"}.join(' ')
+    text_array << custom_hashtags(entry)
 
     text = text_array.join("\n\n")
     image_url = if entry.photos.first.is_vertical?
@@ -31,4 +17,21 @@ class InstagramJob < BufferJob
 
     post_to_buffer('instagram', text, image_url, thumbnail_url)
   end
+
+ private
+ # Checks the entry's tags; if it includes any of the keys in the hashtags.yml
+ # as a tag, then it appends the list under that key as additional hashtags
+ # for instagram
+ def custom_hashtags(entry)
+   entry_tags = entry.combined_tags.map { |t| t.slug.gsub(/-/, '') }
+   custom_hashtags = YAML.load_file(Rails.root.join('config/hashtags.yml'))['instagram']
+   custom_hashtags.each do |k, v|
+     if k == 'all'
+       entry_tags += custom_hashtags[k]
+     elsif entry_tags.include? k
+       entry_tags += custom_hashtags[k]
+     end
+   end
+   entry_tags[0, 30].sort.map { |t| "##{t}"}.join(' ')
+ end
 end
