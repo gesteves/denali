eee04545fe5a83de01e6fc9f4798ba8c7ae22ebd
Guillermo Esteves <g@gesteves.com>
2017-05-18 20:00:28 -0400

    Cache viewport dimensions

--- app/assets/javascripts/application/image_zoom.js
+++ app/assets/javascripts/application/image_zoom.js
@@ -21,15 +21,17 @@ Denali.ImageZoom = (function () {
         original_height,
         image_ratio,
         max_width,
-        height;
+        height,
+        client_height = document.documentElement.clientHeight,
+        client_width = document.documentElement.clientWidth;
     for (var i = 0; i < images.length; i++) {
       image = images[i];
       original_height = parseInt(image.getAttribute('data-height-original'));
       original_width = parseInt(image.getAttribute('data-width-original'));
       image_ratio = original_height/original_width;
-      max_width = Math.min(original_width, document.documentElement.clientWidth, opts.max_width);
+      max_width = Math.min(original_width, client_width, opts.max_width);
       height = max_width * image_ratio;
-      if (height > document.documentElement.clientHeight) {
+      if (height > client_height) {
         image.classList.add(opts.zoomable_class);
         image.addEventListener('click', toggleZoom);
         zoomable.push(image.parentNode.parentNode);
