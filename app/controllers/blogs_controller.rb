1e1a1fdfdf6f07771180f84dc808448c2eb4a544
Guillermo Esteves <g@gesteves.com>
2017-05-28 08:39:43 -0400

    Bump cache in offline assets

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
 class BlogsController < ApplicationController
-  before_action :set_entry_max_age
+  before_action :set_entry_max_age, except: [:offline, :manifest]
   skip_before_action :verify_authenticity_token
 
   def about
@@ -7,9 +7,11 @@ class BlogsController < ApplicationController
   end
 
   def offline
+    expires_in 24.hours, public: true
   end
 
   def manifest
+    expires_in 24.hours, public: true
     @icons = @photoblog.touch_icon.present? ? [128, 152, 144, 192].map { |size| { sizes: "#{size}x#{size}", type: 'image/png', src: @photoblog.touch_icon_url(w: size) } } : []
   end
 end
