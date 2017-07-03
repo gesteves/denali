57eb0dc854256555d8892c072a8d14f4ef13cc2d
Guillermo Esteves <g@gesteves.com>
2017-07-03 10:21:57 -0400

    Handle infinite scrolling pagination

--- app/assets/javascripts/application/infinite_scroll.js
+++ app/assets/javascripts/application/infinite_scroll.js
@@ -2,19 +2,21 @@
 'use strict';
 
 class InfiniteScroll {
-  constructor (container, pagination = '.pagination', footer = '.footer', rootMargin = '100px') {
-    this.pagination = document.querySelector(pagination);
+  constructor () {
+    this.pagination = document.querySelector('.pagination');
     if (!this.pagination) {
       return;
     }
     this.sentinel = this.setUpSentinel(this.pagination);
-    this.container = document.querySelector(container);
-    this.footer = document.querySelector(footer);
+    this.container = document.querySelector('.entry-list');
+    this.footer = document.querySelector('.footer');
     this.footer.style.opacity = 0;
     this.baseUrl = this.container.getAttribute('data-base-url');
     this.currentPage = parseInt(this.container.getAttribute('data-current-page'));
-    this.observer = new IntersectionObserver(entries => this.handleIntersection(entries), { rootMargin: rootMargin });
-    this.observer.observe(this.sentinel);
+    this.loadObserver = new IntersectionObserver(entries => this.checkForNextPage(entries), { rootMargin: '100%' });
+    this.loadObserver.observe(this.sentinel);
+    this.pageObserver = new IntersectionObserver(entries => this.checkPagination(entries), { threshold: 1.0 });
+    this.updatePageSentinels();
     this.loading = false;
   }
 
@@ -25,7 +27,7 @@ class InfiniteScroll {
     return sentinel;
   }
 
-  handleIntersection (entries) {
+  checkForNextPage (entries) {
     entries.forEach(entry => {
       if ((entry.intersectionRatio > 0 || entry.isIntersecting) && !this.loading) {
         this.getNextPage();
@@ -40,14 +42,10 @@ class InfiniteScroll {
     request.onload = () => {
       if (request.status >= 200 && request.status < 400) {
         this.container.insertAdjacentHTML('beforeend', request.responseText);
-        window.history.replaceState(null, null, `${this.baseUrl}/page/${nextPage}`);
         this.currentPage = nextPage;
-        if (typeof ga !== 'undefined') {
-          ga('set', 'page', window.location.pathname);
-          ga('send', 'pageview');
-        }
+        this.updatePageSentinels();
       } else {
-        this.observer.unobserve(this.sentinel);
+        this.loadObserver.unobserve(this.sentinel);
         this.footer.style.opacity = 1;
       }
       this.loading = false;
@@ -55,10 +53,23 @@ class InfiniteScroll {
     this.loading = true;
     request.send();
   }
+
+  updatePageSentinels () {
+    let elements = this.container.querySelectorAll('[data-page-url]');
+    elements.forEach(element => this.pageObserver.observe(element));
+  }
+
+  checkPagination (entries) {
+    entries.forEach(entry => {
+      if ((entry.intersectionRatio > 0 || entry.isIntersecting)) {
+        window.history.replaceState(null, null, entry.target.getAttribute('data-page-url'));
+      }
+    });
+  }
 }
 
 if (document.readyState !== 'loading') {
-  new InfiniteScroll('.entry-list');
+  new InfiniteScroll();
 } else {
-  document.addEventListener('DOMContentLoaded', () => new InfiniteScroll('.entry-list'));
+  document.addEventListener('DOMContentLoaded', () => new InfiniteScroll());
 }
--- app/views/entries/tagged.json.jbuilder
+++ app/views/entries/tagged.json.jbuilder
@@ -1,4 +1,4 @@
-json.cache! "entries/tagged/json/#{@tag_slug}/page/#{@page}/count/#{@count}/#{@photoblog.id}/#{@photoblog.updated_at.to_i}" do
+json.cache! "entries/json/tagged/#{@tag_slug}/page/#{@page}/count/#{@count}/#{@photoblog.id}/#{@photoblog.updated_at.to_i}" do
   json.links do
     json.self @page == 1 ? tag_url(@tag_slug) : tag_url(@tag_slug, @page)
     if @page > 1
