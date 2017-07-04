8e62d8a00ec83558465f1cca378576e2ba65fe0e
Guillermo Esteves <g@gesteves.com>
2017-07-03 21:22:03 -0400

    Increase the dang root margin

--- app/assets/javascripts/application/infinite_scroll.js
+++ app/assets/javascripts/application/infinite_scroll.js
@@ -18,7 +18,7 @@ class InfiniteScroll {
     this.currentPage = parseInt(this.container.getAttribute('data-current-page'));
 
     IntersectionObserver.prototype.POLL_INTERVAL = 10;
-    this.loadingIO = new IntersectionObserver(e => this.loadEntries(e), { rootMargin: '25%' });
+    this.loadingIO = new IntersectionObserver(e => this.loadEntries(e), { rootMargin: '50%' });
     this.loadingIO.observe(this.sentinel);
     this.paginationIO = new IntersectionObserver(e => this.updatePage(e), { threshold: 1.0 });
     this.observePageUrls();
