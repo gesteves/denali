b9231750617e0347b58d7adf08c006f775a2d1d1
Guillermo Esteves <g@gesteves.com>
2017-07-22 18:33:12 -0400

    Reduce root margin

--- app/assets/javascripts/application/infinite_scroll.js
+++ app/assets/javascripts/application/infinite_scroll.js
@@ -30,7 +30,7 @@ class InfiniteScroll {
     this.currentPage = parseInt(this.container.getAttribute('data-current-page'));
 
     IntersectionObserver.prototype.POLL_INTERVAL = 50;
-    this.loadingIO = new IntersectionObserver(e => this.loadEntries(e), { rootMargin: '50%' });
+    this.loadingIO = new IntersectionObserver(e => this.loadEntries(e), { rootMargin: '100px' });
     this.loadingIO.observe(this.sentinel);
     this.paginationIO = new IntersectionObserver(e => this.updatePage(e), { threshold: 1.0 });
     this.observePageUrls();
