ea360ec1046039e0a4fcd52fa2d2bb4b5c78cc94
Guillermo Esteves <g@gesteves.com>
2017-07-22 14:08:41 -0400

    Don’t force lowercase tags

--- config/initializers/acts_as_taggable.rb
+++ config/initializers/acts_as_taggable.rb
@@ -1,5 +1,4 @@
 ActsAsTaggableOn.remove_unused_tags = true
-ActsAsTaggableOn.force_lowercase = true
 ActsAsTaggableOn::Tag.class_eval do
   before_save { |tag| tag.slug = name.parameterize if name_changed? }
 
