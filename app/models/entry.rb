e1acd755bbe3f0a8d4ccf22c7d52299583425ca0
Guillermo Esteves <g@gesteves.com>
2017-08-01 11:39:55 -0400

    Add title again

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -131,9 +131,10 @@ class Entry < ApplicationRecord
             must_not: {
               term: { id: self.id }
             },
-            should: {
+            should: [
+              match: { plain_title: { query: self.title } },
               match: { es_tags: { query: (self.tags + self.locations).map(&:name).join(' ') } }
-            },
+            ],
             minimum_should_match: 1
           }
         },
