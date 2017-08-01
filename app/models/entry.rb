dcef2f527700495e70298cc428365d54670972bc
Guillermo Esteves <g@gesteves.com>
2017-08-01 10:59:56 -0400

    Remove boost

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -132,8 +132,8 @@ class Entry < ApplicationRecord
               match: { id: self.id }
             },
             should: [
-              match: { plain_title: { query: self.plain_title, boost: 2 } },
-              match: { es_tags: { query: (self.tags + self.locations).map(&:name).join(' '), boost: 1 } }
+              match: { plain_title: { query: self.plain_title } },
+              match: { es_tags: { query: (self.tags + self.locations).map(&:name).join(' ') } }
             ],
             minimum_should_match: 1
           }
