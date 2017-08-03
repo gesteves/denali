40cec134d1825f96bbc9ec5f2b44658ce589c32d
Guillermo Esteves <g@gesteves.com>
2017-08-03 19:08:46 -0400

    Lower max age on robots

--- app/controllers/robots_controller.rb
+++ app/controllers/robots_controller.rb
@@ -1,6 +1,6 @@
 class RobotsController < ApplicationController
   def show
-    expires_in 1.year, public: true
+    expires_in 24.hours, public: true
     respond_to do |format|
       format.txt
     end
