a1f210fb7c11e354997c2bc361d5c1d977f921c8
Guillermo Esteves <g@gesteves.com>
2017-08-03 17:09:52 -0400

    Unzoom photos by default

--- app/assets/javascripts/application/image_zoom.js
+++ app/assets/javascripts/application/image_zoom.js
@@ -1,27 +1,36 @@
 'use strict';
 
 class ImageZoom {
-  constructor (imagesSelector, zoomableClass, zoomClass, maxWidth = 1680) {
+  constructor (opts = {}) {
+    const options = Object.assign({
+      imagesSelector: '.entry__photo',
+      zoomableClass: 'entry__photo--zoomable',
+      zoomClass: 'entry__photo-container--zoom',
+      maxWidth: 1680
+    }, opts);
     this.zoomable = [];
-    this.zoomClass = zoomClass;
+    this.zoomClass = options.zoomClass;
     let originalWidth,
         originalHeight,
         imageRatio,
         height,
         clientHeight = document.documentElement.clientHeight,
         clientWidth = document.documentElement.clientWidth,
-        images = document.querySelectorAll(imagesSelector);
+        maxWidth,
+        imageContainer,
+        images = document.querySelectorAll(options.imagesSelector);
 
     images.forEach(image => {
       originalHeight = parseInt(image.getAttribute('data-height-original'));
       originalWidth = parseInt(image.getAttribute('data-width-original'));
       imageRatio = originalHeight/originalWidth;
-      maxWidth = Math.min(originalWidth, clientWidth, maxWidth);
+      maxWidth = Math.min(originalWidth, clientWidth, options.maxWidth);
       height = maxWidth * imageRatio;
+      imageContainer = image.parentNode.parentNode;
       if (height > clientHeight) {
-        image.classList.add(zoomableClass);
+        image.classList.add(options.zoomableClass);
         image.addEventListener('click', e => this.toggleZoom(e));
-        this.zoomable.push(image.parentNode.parentNode);
+        this.zoomable.push(imageContainer);
       }
     });
   }
@@ -33,9 +42,9 @@ class ImageZoom {
 }
 
 if (document.readyState !== 'loading') {
-  new ImageZoom('.entry__photo', 'entry__photo--zoomable', 'entry__photo-container--zoom');
+  new ImageZoom();
 } else {
   document.addEventListener('DOMContentLoaded', () => {
-    new ImageZoom('.entry__photo', 'entry__photo--zoomable', 'entry__photo-container--zoom');
+    new ImageZoom();
   });
 }
