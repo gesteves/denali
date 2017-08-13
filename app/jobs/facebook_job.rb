76eeeacdb1089a5446a90c0e8e126923e541c9c1
Guillermo Esteves <g@gesteves.com>
2017-08-13 11:20:07 -0400

    Add body and tags to Facebook posts

--- app/jobs/facebook_job.rb
+++ app/jobs/facebook_job.rb
@@ -2,9 +2,13 @@ class FacebookJob < BufferJob
   queue_as :default
 
   def perform(entry)
-    text = "#{entry.plain_title}\n\n#{entry.permalink_url(utm_source: 'facebook.com', utm_medium: 'social')}"
+    text = []
+    text << entry.plain_title
+    text << entry.plain_body if entry.body.present?
+    text << entry.permalink_url(utm_source: 'facebook.com', utm_medium: 'social')
+    text << entry.combined_tags.map { |t| "##{t.slug.gsub(/-/, '')}" }.join(' ')
     image_url = entry.photos.first.url(w: 2048, fm: 'jpg')
     thumbnail_url = entry.photos.first.url(w: 512, fm: 'jpg')
-    post_to_buffer('facebook', text, image_url, thumbnail_url)
+    post_to_buffer('facebook', text.join("\n\n"), image_url, thumbnail_url)
   end
 end
