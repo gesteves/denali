3f2e4528fea86c47bea99be3265d7ba03ea1d7c9
Guillermo Esteves <g@gesteves.com>
2017-08-13 17:53:05 -0400

    Don’t use hashtags in facebook

--- app/jobs/facebook_job.rb
+++ app/jobs/facebook_job.rb
@@ -6,7 +6,6 @@ class FacebookJob < BufferJob
     text << entry.plain_title
     text << entry.plain_body if entry.body.present?
     text << entry.permalink_url(utm_source: 'facebook.com', utm_medium: 'social')
-    text << entry.combined_tags.map { |t| "##{t.slug.gsub(/-/, '')}" }.join(' ')
     image_url = entry.photos.first.url(w: 2048, fm: 'jpg')
     thumbnail_url = entry.photos.first.url(w: 512, fm: 'jpg')
     post_to_buffer('facebook', text.join("\n\n"), image_url, thumbnail_url)
