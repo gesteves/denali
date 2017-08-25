40fd32e9a59fe7ed202b99682fe48a1f5517e5f5
Guillermo Esteves <g@gesteves.com>
2017-08-25 12:53:50 -0400

    Update elasticsearch.rb

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -2,7 +2,7 @@ if ENV['ELASTICSEARCH_URL'].present?
   Elasticsearch::Model.client = Elasticsearch::Client.new({
     host: ENV['ELASTICSEARCH_URL'],
     transport_options: {
-      request: { timeout: ENV['ELASTICSEARCH_TIMEOUT'] || 1 }
+      request: { timeout: ENV['ELASTICSEARCH_TIMEOUT'].to_i || 1 }
     }
   })
 end
