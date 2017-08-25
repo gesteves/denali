9d36c2aa58d8c078ecec05b6a43278bb38f00d0b
Guillermo Esteves <g@gesteves.com>
2017-08-25 12:52:25 -0400

    Set elastic search timeout in env var

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -2,7 +2,7 @@ if ENV['ELASTICSEARCH_URL'].present?
   Elasticsearch::Model.client = Elasticsearch::Client.new({
     host: ENV['ELASTICSEARCH_URL'],
     transport_options: {
-      request: { timeout: 1 }
+      request: { timeout: ENV['ELASTICSEARCH_TIMEOUT'] || 1 }
     }
   })
 end
