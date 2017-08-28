5ae9db615e211e653148d43cbcb1d970566caf5b
Guillermo Esteves <g@gesteves.com>
2017-08-28 16:04:05 -0400

    Update facebook_job.rb

--- app/jobs/facebook_job.rb
+++ app/jobs/facebook_job.rb
@@ -6,18 +6,16 @@ class FacebookJob < BufferJob
     text << entry.plain_title
     text << entry.plain_body if entry.body.present?
     text << entry.permalink_url(utm_source: 'facebook.com', utm_medium: 'social')
+    text = text.join("\n\n")
     image_url = entry.photos.first.url(w: 2048, fm: 'jpg')
     thumbnail_url = entry.photos.first.url(w: 512, fm: 'jpg')
 
     # Post to facebook profiles
     ids = get_profile_ids('facebook')
-    post_to_buffer(ids, text.join("\n\n"), image_url, thumbnail_url)
-
-    # Add tags for facebook page
-    text << entry.combined_tags.map { |t| "##{t.slug.gsub(/-/, '')}" }.join(' ')
+    post_to_buffer(ids, text, image_url, thumbnail_url)
 
     # Post to facebook pages
     ids = get_profile_ids('facebook', 'page')
-    post_to_buffer(ids, text.join("\n\n"), image_url, thumbnail_url)
+    post_to_buffer(ids, text, image_url, thumbnail_url)
   end
 end
