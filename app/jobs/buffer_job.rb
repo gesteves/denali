d67845c0d236f0af02092e8764e6e95277354465
Guillermo Esteves <g@gesteves.com>
2017-09-03 09:48:49 -0400

    Cache buffer profiles

--- app/jobs/buffer_job.rb
+++ app/jobs/buffer_job.rb
@@ -2,13 +2,16 @@ class BufferJob < ApplicationJob
   private
 
   def get_profile_ids(service, service_type = 'profile')
-    response = HTTParty.get("https://api.bufferapp.com/1/profiles.json?access_token=#{ENV['buffer_access_token']}")
-    if response.code >= 400
-      raise response.body
-    else
-      profiles = JSON.parse(response.body)
-      profiles.select { |profile| profile['service'].downcase.match(service) && profile['service_type'].downcase.match(service_type) }.map { |profile| profile['id'] }
+    profiles = Rails.cache.fetch('buffer:profiles', expires_in: 1.hour) do
+      response = HTTParty.get("https://api.bufferapp.com/1/profiles.json?access_token=#{ENV['buffer_access_token']}")
+      if response.code >= 400
+        raise response.body
+      else
+        response.body
+      end
     end
+    profiles = JSON.parse(response.body)
+    profiles.select { |profile| profile['service'].downcase.match(service) && profile['service_type'].downcase.match(service_type) }.map { |profile| profile['id'] }
   end
 
   def post_to_buffer(profile_ids, text, image_url, thumbnail_url)
