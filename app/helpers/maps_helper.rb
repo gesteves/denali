0fec13f37522905e501616e4c055552bcb89a30b
Guillermo Esteves <g@gesteves.com>
2018-01-05 11:34:42 -0500

    Fix map popups

--- app/helpers/maps_helper.rb
+++ app/helpers/maps_helper.rb
@@ -1,19 +1,21 @@
 module MapsHelper
   def tooltip_content(photo, entry)
     content_tag :div, class: 'entry-list__item entry-list__item--map' do
-      link_to entry.permalink_url, { class: 'entry-list__link entry-list__link--photo', target: 'blank' } do
-        content_tag :figure, class: 'entry-list__photo' do
-          img = responsive_image_tag(photo, 'map', { class: 'entry-list__image' })
-          caption = content_tag :figcaption, class: 'entry-list__photo-caption' do
-            title = content_tag :p, class: 'entry-list__photo-title' do
-              raw truncate(entry.plain_title, length: 80, separator: ' ', omission: '…', escape: false)
+      content_tag :div, class: 'entry-list__wrapper' do
+        link_to entry.permalink_url, { class: 'entry-list__link entry-list__link--photo', target: 'blank' } do
+          content_tag :figure, class: 'entry-list__photo' do
+            img = responsive_image_tag(photo, 'map', { class: 'entry-list__image' })
+            caption = content_tag :figcaption, class: 'entry-list__photo-caption' do
+              title = content_tag :p, class: 'entry-list__photo-title' do
+                raw truncate(entry.plain_title, length: 80, separator: ' ', omission: '…', escape: false)
+              end
+              pubdate = content_tag :p, class: 'entry-list__photo-meta' do
+                "Published on #{entry.published_at.strftime('%B %-d, %Y')}"
+              end
+              title + pubdate
             end
-            pubdate = content_tag :p, class: 'entry-list__photo-meta' do
-              "Published on #{entry.published_at.strftime('%B %-d, %Y')}"
-            end
-            title + pubdate
+            img + caption
           end
-          img + caption
         end
       end
     end
