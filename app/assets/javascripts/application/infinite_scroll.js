381c9a830bd8d443a09c1860b0137f783dba4cba
Guillermo Esteves <g@gesteves.com>
2018-01-16 10:53:40 -0500

    Use requestIdleCallback for analytics

--- app/assets/javascripts/application/infinite_scroll.js
+++ app/assets/javascripts/application/infinite_scroll.js
@@ -109,14 +109,22 @@ class InfiniteScroll {
       previous_path = window.location.pathname;
       window.history.replaceState(null, null, entry.target.getAttribute('data-page-url'));
       if (previous_path !== window.location.pathname) {
-        if (typeof ga !== 'undefined') {
-          ga('set', 'page', window.location.pathname);
-          ga('send', 'pageview');
-        }
-        if (typeof gtag !== 'undefined' && typeof gaTrackingId !== 'undefined') {
-          gtag('config', gaTrackingId, { 'page_path': window.location.pathname });
+        if ('requestIdleCallback' in window) {
+          requestIdleCallback(() => this.sendAnalytics());
+        } else {
+          this.sendAnalytics();
         }
       }
+    }
+  }
+
+  sendAnalytics () {
+    if (typeof ga !== 'undefined') {
+      ga('set', 'page', window.location.pathname);
+      ga('send', 'pageview');
+    }
+    if (typeof gtag !== 'undefined' && typeof gaTrackingId !== 'undefined') {
+      gtag('config', gaTrackingId, { 'page_path': window.location.pathname });
     }
   }
 }
