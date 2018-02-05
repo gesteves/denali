516886e3e58ae078a1268bc5f9be72da6674dba7
Guillermo Esteves <g@gesteves.com>
2018-02-04 21:15:17 -0500

    Tweak job

--- app/jobs/image_annotation_job.rb
+++ app/jobs/image_annotation_job.rb
@@ -3,15 +3,12 @@ class ImageAnnotationJob < ApplicationJob
 
   def perform(photo)
     response = request_annotations(photo)
-    label_array = response['responses'].try(:first).try(:[], 'labelAnnotations')
-    color_array = response['responses'].try(:first).dig('imagePropertiesAnnotation', 'dominantColors', 'colors')
-    labels = extract_labels(label_array)
-    dominant_color = extract_dominant_color(color_array)
-    is_color = is_color? label_array
+    response_labels = response['responses'].try(:first).try(:[], 'labelAnnotations')
+    response_colors = response['responses'].try(:first).dig('imagePropertiesAnnotation', 'dominantColors', 'colors')
 
-    photo.keywords = labels
-    photo.dominant_color = dominant_color
-    photo.color = is_color
+    photo.keywords = labels(response_labels)
+    photo.dominant_color = dominant_color(response_colors)
+    photo.color = is_color?(response_labels)
     photo.save
   end
 
@@ -40,19 +37,20 @@ class ImageAnnotationJob < ApplicationJob
     JSON.parse(request.body)
   end
 
-  def extract_labels(labels, opts = {})
-    opts.reverse_merge!(min_score: 0.85)
-    return if labels.blank?
+  def labels(labels, opts = {})
+    opts.reverse_merge!(min_score: 0.8)
+    return if labels.blank? || labels.select { |l| l['score'] >= opts[:min_score] }.empty?
     labels.select { |l| l['score'] >= opts[:min_score] }.map { |l| l['description'] }.join(', ')
   end
 
-  def extract_dominant_color(colors)
+  def dominant_color(colors)
     return if colors.blank?
     color = colors.sort { |a, b| b['score'] <=> a['score'] }.first['color']
     to_hex(color)
   end
 
   def to_hex(color)
+    return if color['red'].blank? || color['green'].blank? || color['blue'].blank?
     red = color['red'].to_s(16).rjust(2, '0')
     green = color['green'].to_s(16).rjust(2, '0')
     blue = color['blue'].to_s(16).rjust(2, '0')
@@ -60,7 +58,7 @@ class ImageAnnotationJob < ApplicationJob
   end
 
   def is_color?(labels, opts = {})
-    opts.reverse_merge!(min_score: 0.85)
-    labels.select { |l| l['score'] >= opts[:min_score] }.select { |l| l['description'].match? /(black and white|monochrome)/ }.empty?
+    return false if labels.blank?
+    labels.select { |l| l['description'].match? /(black and white|monochrome)/i }.empty?
   end
 end
