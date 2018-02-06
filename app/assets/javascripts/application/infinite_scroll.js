ccaf1d8d74fb8561650f813e4c80020799859305
Guillermo Esteves <g@gesteves.com>
2018-02-06 08:48:47 -0500

    Fix masonry if there’s only one page

--- app/assets/javascripts/application/infinite_scroll.js
+++ app/assets/javascripts/application/infinite_scroll.js
@@ -12,25 +12,25 @@ class InfiniteScroll {
       footerSelector: '.footer',
       activeClass: 'loading--active'
     }, opts);
-    let pagination = document.querySelector(options.paginationSelector);
-    let container = document.querySelector(options.containerSelector);
-    let sentinel = document.querySelector(options.sentinelSelector);
 
-    if (!container || !sentinel || !pagination) {
+    // These elements are required for the infinite scroll to function:
+    // - `container` is the list of entries on the page.
+    // - `pagination` are the "next" and "previous" links on the page, only present
+    //    if there's more than one page of entries and used as a fallback if JS fails.
+    // - `sentinel` is the element at the bottom of the page that is used both as a
+    //    loading spinner as new pages are loading, and as a target for the intersection
+    //    observer to check if we've reached the bottom of the page, and thus should
+    //    load the next page of entries.
+    this.container = document.querySelector(options.containerSelector);
+    this.pagination = document.querySelector(options.paginationSelector);
+    this.sentinel = document.querySelector(options.sentinelSelector);
+
+    // Early return if none of these elements are present for some reason
+    if (!this.container || !this.sentinel || !this.pagination) {
       return;
     }
 
-    this.container = container;
-    this.sentinel = sentinel;
-    this.sentinel.classList.add(options.activeClass);
-    this.footer = document.querySelector(options.footerSelector);
-    this.footer.style.display = 'none';
-    this.loadingClass = options.loadingClass;
-    pagination.parentNode.removeChild(pagination);
-
-    this.baseUrl = this.container.getAttribute('data-base-url');
-    this.currentPage = parseInt(this.container.getAttribute('data-current-page'));
-
+    // Set up basic masonry layout
     this.masonry = new Masonry(this.container, {
       initLayout: false,
       itemSelector: options.itemSelector,
@@ -42,6 +42,33 @@ class InfiniteScroll {
         opacity: 1
       }
     });
+
+    // If for some reason there's no sentinel, there's nothing to observe.
+    // If there's no pagination, it means there are no more pages to load.
+    // In either of these cases, return early but init the masonry layout first.
+    if (!this.sentinel || !this.pagination) {
+      this.masonry.layout();
+      return;
+    }
+
+    // Set up the bottom of the page for lazy loading: show the sentinel,
+    // hide the footer, remove the pagination links.
+    this.sentinel.classList.add(options.activeClass);
+    this.footer = document.querySelector(options.footerSelector);
+    this.footer.style.display = 'none';
+    this.loadingClass = options.loadingClass;
+    this.pagination.parentNode.removeChild(this.pagination);
+
+    // Store the base URL of the page and the current page number so we can
+    // update the URL in the address bar later.
+    this.baseUrl = this.container.getAttribute('data-base-url');
+    this.currentPage = parseInt(this.container.getAttribute('data-current-page'));
+
+    // Set up an event handler so that once the masonry layout is complete,
+    // it sets up separate intersection observers:
+    // one observes the sentinel at the bottom of the page, and loads more pages;
+    // the other observes the first item of each page,
+    // which updates the URL in the address bar as we scroll.
     this.masonry.once('layoutComplete', () => {
       IntersectionObserver.prototype.POLL_INTERVAL = 50;
       this.loadingObserver = new IntersectionObserver(e => this.handlePageBottom(e), { rootMargin: '25%' });
@@ -49,9 +76,13 @@ class InfiniteScroll {
       this.paginationObserver = new IntersectionObserver(e => this.updatePagePath(e), { threshold: 1.0 });
       this.observePageUrls();
     });
+
+    // Finally, init the masonry layout.
     this.masonry.layout();
   }
 
+  // Handler for the intersection observer that observes the sentinel at the
+  // bottom of the page. If it's visible, loads the next page.
   handlePageBottom (entries) {
     let intersecting = entries.filter(entry => {
       return (entry.intersectionRatio > 0 || entry.isIntersecting);
@@ -62,6 +93,9 @@ class InfiniteScroll {
     }
   }
 
+  // Makes an Ajax request for the next page of results, and either inserts it
+  // into the DOM, or ends the infinite scroll process if there are no
+  // more results (if so, server returns 404).
   getPage (page) {
     let request = new XMLHttpRequest();
     request.open('GET', `${this.baseUrl}/page/${page}.js`, true);
@@ -75,6 +109,8 @@ class InfiniteScroll {
     request.send();
   }
 
+  // Creates an HTML fragment from a string of markup, appends it to the container,
+  // adjusts the masonry, and observes the new elements.
   appendPage (page, html) {
     let fragment = document.createRange().createContextualFragment(html);
     let children = Array.from(fragment.children);
@@ -84,12 +120,18 @@ class InfiniteScroll {
     this.observePageUrls();
   }
 
+  // If there are no more entries to be fetched, unobserves the sentinel,
+  // shows the footer again, and removes the sentinel from the page.
   endInfiniteScroll () {
     this.loadingObserver.unobserve(this.sentinel);
     this.footer.style.display = 'block';
     this.sentinel.parentNode.removeChild(this.sentinel);
   }
 
+  // The first element of each page fetched contains a `data-page-url` data attribute
+  // with the full URL of its parent page. This function makes the intersection observer
+  // observe these new elements as they're added so we can update the URL in the
+  // browser as we scroll.
   observePageUrls () {
     let elements = this.container.querySelectorAll('[data-page-url]');
     for (let i = 0; i < elements.length; i++) {
@@ -98,6 +140,11 @@ class InfiniteScroll {
     }
   }
 
+  // Handler for the pagination intersection observer, which observes the first
+  // element of each new page and checks their `data-page-url` data attribute.
+  // As each element is in view, this function uses that data attribute to
+  // update the URL in the browser address bar using the History API, and
+  // tracks the page view in Analytics if the page changes.
   updatePagePath (entries) {
     let entry,
         previous_path;
@@ -118,6 +165,7 @@ class InfiniteScroll {
     }
   }
 
+  // Makes a Google Analytics page view tracking call.
   trackPageView () {
     if (typeof ga !== 'undefined') {
       ga('set', 'page', window.location.pathname);
