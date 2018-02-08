f051f0456b0b90f11479f41498305b1f9fd51bac
Guillermo Esteves <g@gesteves.com>
2018-02-07 19:25:33 -0500

    Don’t swallow exceptions

--- app/jobs/image_annotation_job.rb
+++ app/jobs/image_annotation_job.rb
@@ -8,21 +8,17 @@ class ImageAnnotationJob < ApplicationJob
   end
 
   def rekognize(photo)
-    begin
-      credentials = Aws::Credentials.new(ENV['aws_access_key_id'], ENV['aws_secret_access_key'])
-      client = Aws::Rekognition::Client.new(credentials: credentials, region: 'us-east-1')
-      opts = {
-        image: {
-          s3_object: {
-            bucket: ENV['s3_bucket'],
-            name: photo.image.s3_object.key
-          }
-        },
-        min_confidence: 80
-      }
-      client.detect_labels(opts).labels
-    rescue
-      []
-    end
+    credentials = Aws::Credentials.new(ENV['aws_access_key_id'], ENV['aws_secret_access_key'])
+    client = Aws::Rekognition::Client.new(credentials: credentials, region: 'us-east-1')
+    opts = {
+      image: {
+        s3_object: {
+          bucket: ENV['s3_bucket'],
+          name: photo.image.s3_object.key
+        }
+      },
+      min_confidence: 80
+    }
+    client.detect_labels(opts).labels
   end
 end
