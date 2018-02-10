deb8b4e06f755c4f3d507da0d0dc1a5e0e83cb98
Guillermo Esteves <g@gesteves.com>
2018-02-10 12:44:36 -0500

    Move infinite load to entry list controller

---
+++ app/frontend/lib/analytics.js
@@ -0,0 +1,20 @@
+// Makes a page view tracking call
+export function trackPageView () {
+  if ('requestIdleCallback' in window) {
+    requestIdleCallback(trackPage);
+  } else {
+    trackPage();
+  }
+
+}
+
+// Makes the actual page view tracking call, in GA or GTM
+function trackPage () {
+  if (typeof ga !== 'undefined') {
+    ga('set', 'page', window.location.pathname);
+    ga('send', 'pageview');
+  }
+  if (typeof gtag !== 'undefined' && typeof gaTrackingId !== 'undefined') {
+    gtag('config', gaTrackingId, { 'page_path': window.location.pathname });
+  }
+}
--- app/frontend/packs/entries/show.js
+++
@@ -1,3 +0,0 @@
-import LazyLoad  from '../../modules/lazy_load.js';
-
-new LazyLoad('js-lazy-load');
