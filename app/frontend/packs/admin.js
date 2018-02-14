949b72f70d47fff5024607c8ea6c5fae7c16ca12
Guillermo Esteves <g@gesteves.com>
2018-02-14 07:38:18 -0500

    Include rails-ujs in admin pack

--- app/frontend/packs/admin.js
+++ app/frontend/packs/admin.js
@@ -1,6 +1,8 @@
 import { Application } from 'stimulus';
 import { definitionsFromContext } from 'stimulus/webpack-helpers';
+import Rails from 'rails-ujs';
 
 const application = Application.start();
 const context = require.context('controllers/admin', true, /.js$/);
 application.load(definitionsFromContext(context));
+Rails.start();
