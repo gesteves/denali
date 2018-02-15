5029f9aca5bdf6813bf254d02f4393a4d0b5457a
Guillermo Esteves <g@gesteves.com>
2018-02-15 07:10:49 -0500

    Fix pagination

--- app/frontend/lib/analytics.js
+++ app/frontend/lib/analytics.js
@@ -1,24 +1,10 @@
 /* global ga, gtag, gaTrackingId */
 /**
- * Checks if `requestIdleCallback` is supported before making a call to track
- * the current page in analytics.
- * https://developer.mozilla.org/en-US/docs/Web/API/Window/requestIdleCallback
- */
-export function trackPageView () {
-  if ('requestIdleCallback' in window) {
-    requestIdleCallback(trackPage);
-  } else {
-    trackPage();
-  }
-}
-
-
-/**
  * Product-agnostic function to make a page view tracking call.
  * Currently supports GA, either directly or as part of GTM.
  * @param {string} path The pathname for the page to be tracked.
  */
-function trackPage (path) {
+export function trackPageView (path) {
   if (typeof ga !== 'undefined') {
     ga('set', 'page', path);
     ga('send', 'pageview');
