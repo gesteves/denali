45e4cace0474e2031e958a281d882a0578751eec
Guillermo Esteves <g@gesteves.com>
2018-04-14 17:08:44 -0400

    Remove counts

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,23 +1,2 @@
 module AdminHelper
-
-  def published_count(opts = {})
-    render_count Entry.published.count, opts
-  end
-
-  def drafts_count(opts = {})
-    render_count Entry.drafted.count, opts
-  end
-
-  def queued_count(opts = {})
-    render_count Entry.queued.count, opts
-  end
-
-  def tag_count(opts = {})
-    render_count ActsAsTaggableOn::Tag.count, opts
-  end
-
-  private
-  def render_count(entries, opts = {})
-    entries > 0 ? content_tag(:span, "&nbsp;·&nbsp;#{number_with_delimiter entries}".html_safe, opts) : ''
-  end
 end
