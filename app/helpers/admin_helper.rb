7cc85948104bb2546f57f42e1567a428ee170b2f
Guillermo Esteves <g@gesteves.com>
2018-04-15 10:01:56 -0400

    Style share page

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -2,7 +2,7 @@ module AdminHelper
   def icon(text, icon, opts = {})
     opts.reverse_merge!(size: 'is-small', additional_classes: '')
     icon = content_tag :span, class: "icon #{opts[:size]} #{opts[:additional_classes]}" do
-      content_tag :i, nil, class: "fas fa-#{icon}"
+      content_tag :i, nil, class: icon
     end
     text = content_tag :span, text
     "#{icon}\n#{text}".html_safe
