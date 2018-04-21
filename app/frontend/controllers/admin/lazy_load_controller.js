365fbee777cdb062c29d169dcfdf100818c9dff3
Guillermo Esteves <g@gesteves.com>
2018-04-21 13:00:06 -0400

    Lazy load admin images

---
+++ app/frontend/controllers/admin/lazy_load_controller.js
@@ -0,0 +1,16 @@
+import { Controller } from 'stimulus';
+import LazyLoad from '../../observers/lazy_load';
+
+/**
+ * Controls the lazy loading of images on the page.
+ * @extends Controller
+ */
+export default class extends Controller {
+  connect () {
+    LazyLoad.observe(this.element);
+  }
+
+  disconnect () {
+    LazyLoad.unobserve(this.element);
+  }
+}
