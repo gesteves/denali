2820c139b36991e4452d3deabc5dc9d95b280983
Guillermo Esteves <g@gesteves.com>
2018-04-22 16:51:22 -0400

    Show only one notification at a time

--- app/frontend/controllers/admin/notifications_controller.js
+++ app/frontend/controllers/admin/notifications_controller.js
@@ -15,13 +15,28 @@ export default class extends Controller {
   show (event) {
     const status = event.detail.status;
     const message = event.detail.message;
-    const notification = $(`<div class="notification is-${status}" data-target="notifications.notification" data-action="click->notifications#close">
+    const notification = $(`<div class="notification is-${status} is-transparent" data-target="notifications.notification" data-action="click->notifications#close transitionend->notifications#toggle">
     <button class="delete"></button>
     ${message}</div>`);
-    if (this.notificationTargets.length === 5) {
-      this.notificationTargets[0].remove();
+    if (this.hasNotificationTarget) {
+      this.closeAll();
+      $(this.containerTarget).prepend(notification);
+    } else {
+      $(this.containerTarget).prepend(notification);
+      this.toggle();
     }
-    $(this.containerTarget).append(notification);
+  }
+
+  /**
+   * Hides all the existing notifications on the page.
+   */
+  closeAll () {
+    console.log('Closing notifications')
+    this.notificationTargets
+      .filter(notification => !notification.classList.contains('is-transparent'))
+      .forEach(notification => {
+        notification.classList.add('is-transparent', 'notification-closed');
+      });
   }
 
   /**
@@ -29,6 +44,19 @@ export default class extends Controller {
    * @param {Event} event Click event from the close button.
    */
   close (event) {
-    event.currentTarget.remove();
+    event.currentTarget.classList.add('is-transparent', 'notification-closed');
+  }
+
+  /**
+   * Removes all notifications that have been marked as closed from the DOM,
+   * and displays any pending transparent notifications.
+   * @param {Event} event Click event from the close button.
+   */
+  toggle () {
+    this.notificationTargets
+      .filter(notification => notification.classList.contains('notification-closed'))
+      .forEach(notification => notification.remove());
+    this.notificationTargets
+      .forEach(notification => setTimeout(() => notification.classList.remove('is-transparent'), 10));
   }
 }
