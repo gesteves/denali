9e5424a208f6a4a450dcd1d07b70129b6a1abeec
Guillermo Esteves <g@gesteves.com>
2018-05-14 21:04:47 -0400

    Handle exceptions creating new articles

--- app/jobs/apple_news_job.rb
+++ app/jobs/apple_news_job.rb
@@ -6,7 +6,13 @@ class AppleNewsJob < ApplicationJob
       return if !entry.is_published? || ENV['apple_news_channel_id'].blank? || ENV['apple_news_key_id'].blank? || ENV['apple_news_secret'].blank?
 
       document = generate_document(entry)
-      article = AppleNews::Article.new(entry.apple_news_id, document: document)
+      begin
+        article = AppleNews::Article.new(entry.apple_news_id, document: document)
+      rescue NoMethodError
+        # This means an article with that apple_news_id doesn't exist,
+        # (likely deleted in News Publisher), so just create a new article.
+        article = AppleNews::Article.new
+      end
 
       response = article.save!
       if response.is_a? Array
