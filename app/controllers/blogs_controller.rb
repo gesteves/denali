fe74103a14f621889324ce5d79d12b2f12e52010
Guillermo Esteves <g@gesteves.com>
2018-05-19 20:21:18 -0400

    Tweak manifest

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -12,6 +12,6 @@ class BlogsController < ApplicationController
 
   def manifest
     expires_in 24.hours, public: true
-    @icons = @photoblog.touch_icon.present? ? [128, 152, 144, 192].map { |size| { sizes: "#{size}x#{size}", type: 'image/png', src: @photoblog.touch_icon_url(w: size) } } : []
+    @icons = @photoblog.touch_icon.present? ? [128, 152, 144, 192, 512].map { |size| { sizes: "#{size}x#{size}", type: 'image/png', src: @photoblog.touch_icon_url(w: size) } } : []
   end
 end
