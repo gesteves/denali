ea12e6585fd25cce5620c4a8b8dcf79b30bcae64
Guillermo Esteves <g@gesteves.com>
2018-06-23 19:16:55 -0400

    Unregister service workers

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -5,3 +5,11 @@ import { definitionsFromContext } from 'stimulus/webpack-helpers';
 const application = Application.start();
 const context = require.context('controllers/application', true, /.js$/);
 application.load(definitionsFromContext(context));
+
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.getRegistrations().then(function(registrations) {
+    for (let registration of registrations) {
+      registration.unregister();
+    }
+  });
+}
