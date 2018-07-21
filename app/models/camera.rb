ecf09624fa59b26bc97f65ffdc0cda708cb7036c
Guillermo Esteves <g@gesteves.com>
2018-07-21 16:11:19 -0400

    Only show exif if camera is present

--- app/models/camera.rb
+++ app/models/camera.rb
@@ -1,4 +1,8 @@
 class Camera < ApplicationRecord
   has_many :photos
   validates :slug, uniqueness: true
+
+  def article
+    %w(a e i o u).include?(self.display_name[0].downcase) ? 'an' : 'a'
+  end
 end
