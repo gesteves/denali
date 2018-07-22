dd36c357738ded362f60971565340faa2e705609
Guillermo Esteves <g@gesteves.com>
2018-07-22 15:20:52 -0400

    Don’t tag lenses

--- app/jobs/entry_tag_update_job.rb
+++ app/jobs/entry_tag_update_job.rb
@@ -6,7 +6,7 @@ class EntryTagUpdateJob < ApplicationJob
     location_tags = []
     style_tags = []
     entry.photos.each do |p|
-      equipment_tags << [p.camera&.make, p.camera&.display_name, p.lens&.display_name, p.film&.display_name]
+      equipment_tags << [p.camera&.make, p.camera&.display_name, p.film&.display_name]
       style_tags << (p.color? ? 'Color' : 'Black and White') unless p.color?.nil?
       style_tags << 'Film' if p.film.present?
       location_tags  << [p.country, p.locality, p.sublocality, p.neighborhood, p.administrative_area] if entry.show_in_map?
