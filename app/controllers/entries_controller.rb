6d3ef21d55b3d30d927ceb938818dc0ab6e25d9a
Guillermo Esteves <guillermo@voxmedia.com>
2018-07-25 20:38:55 -0400

    Remove preload

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -77,7 +77,6 @@ class EntriesController < ApplicationController
     if stale?(@entry, public: true)
       respond_to do |format|
         format.html {
-          add_preload_link_header(related_url(@entry, format: 'js'), as: 'fetch', crossorigin: 'anonymous')
           redirect_to(@entry.permalink_url, status: 301) unless params_match(@entry, params)
         }
         format.json
@@ -103,7 +102,6 @@ class EntriesController < ApplicationController
     if stale?(@entry, public: true)
       respond_to do |format|
         format.html {
-          add_preload_link_header(related_url(@entry, format: 'js'), as: 'fetch', crossorigin: 'anonymous')
           if @entry.is_published?
             redirect_to @entry.permalink_url
           else
