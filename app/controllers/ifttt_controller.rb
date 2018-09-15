d19c69d314a91ff1c46f2f4df957ef1e5a8d5afe
Guillermo Esteves <g@gesteves.com>
2018-09-15 14:12:50 -0400

    Post new instagram photos in Slack

--- app/controllers/ifttt_controller.rb
+++ app/controllers/ifttt_controller.rb
@@ -6,16 +6,21 @@ class IftttController < ApplicationController
     if params[:token] != ENV['ifttt_webhook_token']
       render plain: 'No', status: 401
     else
-      logger.tagged('IFTTT', 'WEBHOOK') { logger.info params[:created_at] }
-      logger.tagged('IFTTT', 'WEBHOOK') { logger.info params[:source_url] }
-      logger.tagged('IFTTT', 'WEBHOOK') { logger.info params[:url] }
       entry = Entry.published.first
-      hashtags = entry.instagram_hashtags
-      payload = {
-        value1: hashtags
+
+      attachment = {
+        fallback: 'New photo on Instagram!',
+        text: params[:caption],
+        image_url: params[:source_url],
+        actions: [{
+          type: 'button',
+          text: 'Open in Instagram',
+          url: params[:url]
+        }]
       }
-      SlackJob.perform_later(text: hashtags)
-      IftttWebhookJob.perform_later('instagram_tags', payload.to_json)
+
+      SlackJob.perform_later(attachments: [attachment])
+      SlackJob.perform_later(text: entry.instagram_hashtags)
       render plain: 'OK'
     end
   end
