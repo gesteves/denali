fcbfedd382b0ef9a306c375d23a0400bd4d5600c
Guillermo Esteves <g@gesteves.com>
2018-09-15 10:22:33 -0400

    Add support for IFTTT webhooks

---
+++ app/controllers/ifttt_controller.rb
@@ -0,0 +1,18 @@
+class IftttController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def instagram
+    if params[:token] != ENV['ifttt_webhook_token']
+      render plain: 'No', status: 401
+    else
+      entry = Entry.published.first
+      payload = {
+        value1: entry.plain_title,
+        value2: share_admin_entry_url(entry, host: "#{ENV['HEROKU_APP_NAME']}.herokuapp.com", protocol: 'https'),
+        value3: entry.photos.first.url(w: 1200, fm: 'jpg')
+      }
+      IftttWebhookJob.perform_later('instagram_tags', payload.to_json)
+      render plain: 'OK'
+    end
+  end
+end
