e62bd7eca6632a8076095fbf0dab401fd3c3fc9b
Guillermo Esteves <g@gesteves.com>
2019-01-29 08:45:43 -0700

    Update publish_schedule.rb

--- app/models/publish_schedule.rb
+++ app/models/publish_schedule.rb
@@ -2,4 +2,10 @@ class PublishSchedule < ApplicationRecord
   belongs_to :blog, touch: true, counter_cache: true, optional: true
 
   validates :hour, uniqueness: true
+  
+  after_save :update_blog
+  
+  def update_blog
+    self.blog.touch
+  end
 end
