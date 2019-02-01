08544bf2dfc1a0c8c07faf2236d7559b37909792
Guillermo Esteves <g@gesteves.com>
2019-02-01 07:25:41 -0700

    Remove unused helpers

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -11,16 +11,4 @@ module AdminHelper
   def pluralize_with_delimiter(word, count)
     "#{number_with_delimiter count} #{word.pluralize(count)}"
   end
-
-  def intrinsic_ratio_padding(photo)
-    return '' if photo.width.blank? || photo.height.blank?
-    padding = (photo.height.to_f/photo.width.to_f) * 100
-    "padding-top:#{padding}%".html_safe
-  end
-
-  def image_placeholder(photo)
-    return '' if photo.color_palette.blank?
-    palette = photo.color_palette.split(',').sample(2).join(',')
-    "background:linear-gradient(to bottom right, #{palette})".html_safe
-  end
 end
