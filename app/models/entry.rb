fbaa46393c2d068e3ed07bb2d8870852226ca9ed
Guillermo Esteves <g@gesteves.com>
2019-02-09 09:12:52 -0700

    Revert "Fix instagram hashtags"
    This reverts commit f079c4cecd0d50b82e4fc181fb044f21718dcf5d.

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -323,8 +323,8 @@ class Entry < ApplicationRecord
     entry_tags = self.combined_tags.map { |t| t.slug.gsub(/-/, '') }
     instagram_hashtags = YAML.load_file(Rails.root.join('config/hashtags.yml'))['instagram']
 
-    tags = []
-    extra_tags = []
+    tags = instagram_hashtags['featureaccounts']&.sample(5) || []
+    extra_tags = instagram_hashtags['featureaccounts'] || []
 
     # For each entry tag, add 5 matching Instagram tags to the array
     instagram_hashtags.each do |k, v|
