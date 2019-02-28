e1b17d5209c1ceba693cf871ba0396cd4fddc4d7
Guillermo Esteves <g@gesteves.com>
2019-02-27 20:02:53 -0700

    Set up attachments once

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,16 +6,14 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  def setup
-    Photo.all.each do |photo|
-      photo.image.attach(io: File.open(Rails.root.join('test/fixtures/images/rusty.jpg')), filename: 'rusty.jpg')
-    end
+  Photo.all.each do |photo|
+    photo.image.attach(io: File.open(Rails.root.join('test/fixtures/images/rusty.jpg')), filename: 'rusty.jpg')
+  end
 
-    Blog.all.each do |blog|
-      blog.favicon.attach(io: File.open(Rails.root.join('test/fixtures/images/a.png')), filename: 'a.png')
-      blog.touch_icon.attach(io: File.open(Rails.root.join('test/fixtures/images/a.png')), filename: 'a.png')
-      blog.logo.attach(io: File.open(Rails.root.join('test/fixtures/images/a.png')), filename: 'a.png')
-    end
+  Blog.all.each do |blog|
+    blog.favicon.attach(io: File.open(Rails.root.join('test/fixtures/images/a.png')), filename: 'a.png')
+    blog.touch_icon.attach(io: File.open(Rails.root.join('test/fixtures/images/a.png')), filename: 'a.png')
+    blog.logo.attach(io: File.open(Rails.root.join('test/fixtures/images/a.png')), filename: 'a.png')
   end
 
   # Add more helper methods to be used by all tests here...
