63c2f4ebb821c0dc4d11b56b4d90f209f831e75c
Guillermo Esteves <g@gesteves.com>
2019-02-28 17:47:09 -0700

    Use redis url

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,6 +1,6 @@
 if Rails.env.test?
   Resque.redis = MockRedis.new
 else
-  Resque.redis = Redis.new(url: ENV['HEROKU_REDIS_ROSE_URL'] || 'redis://localhost:6379')
+  Resque.redis = Redis.new(url: ENV['REDIS_URL'] || 'redis://localhost:6379')
 end
 Resque.after_fork = Proc.new { ActiveRecord::Base.establish_connection }
