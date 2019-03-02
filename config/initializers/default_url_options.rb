7e0a0155185e847b10fad549651f848142ec8a9d
Guillermo Esteves <g@gesteves.com>
2019-03-02 14:42:08 -0700

    Use correct hostname for review apps

--- config/initializers/default_url_options.rb
+++ config/initializers/default_url_options.rb
@@ -1,12 +1,16 @@
-host = if Rails.env.production?
-  ENV['domain'] || "#{ENV['HEROKU_APP_NAME']}.herokuapp.com"
-else
-  'localhost'
+if Rails.env.production?
+  host = if ENV['HEROKU_PARENT_APP_NAME'].present?
+    "#{ENV['HEROKU_APP_NAME']}.herokuapp.com"
+  elsif ENV['domain'].present?
+    ENV['domain']
+  else
+    "#{ENV['HEROKU_APP_NAME']}.herokuapp.com"
+  end
+
+  protocol = Rails.application.config.force_ssl ? 'https' : 'http'
+
+  Rails.application.routes.default_url_options.merge!(
+    host: host,
+    protocol: protocol,
+  )
 end
-
-protocol = Rails.application.config.force_ssl ? 'https' : 'http'
-
-Rails.application.routes.default_url_options.merge!(
-  host: host,
-  protocol: protocol,
-)
