a720b55e008de72d0b20739fbcfadd4734d378df
Guillermo Esteves <g@gesteves.com>
2019-03-02 14:48:06 -0700

    Set host for other envs

--- config/initializers/default_url_options.rb
+++ config/initializers/default_url_options.rb
@@ -1,16 +1,18 @@
-if Rails.env.production?
-  host = if ENV['HEROKU_PARENT_APP_NAME'].present?
+host = if Rails.env.production?
+  if ENV['HEROKU_PARENT_APP_NAME'].present?
     "#{ENV['HEROKU_APP_NAME']}.herokuapp.com"
   elsif ENV['domain'].present?
     ENV['domain']
   else
     "#{ENV['HEROKU_APP_NAME']}.herokuapp.com"
   end
-
-  protocol = Rails.application.config.force_ssl ? 'https' : 'http'
-
-  Rails.application.routes.default_url_options.merge!(
-    host: host,
-    protocol: protocol,
-  )
+else
+  ENV['domain'] || 'localhost'
 end
+
+protocol = Rails.application.config.force_ssl ? 'https' : 'http'
+
+Rails.application.routes.default_url_options.merge!(
+  host: host,
+  protocol: protocol,
+)
