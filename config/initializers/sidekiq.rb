798fdd635007c4f69227c30f1bb510cb914ef339
Guillermo Esteves <g@gesteves.com>
2019-04-04 17:02:24 -0600

    Tweak sidekiq config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,10 +1,8 @@
-unless Rails.env.test?
-  redis_url = ENV['REDIS_URL'] || 'redis://localhost:6379'
-  Sidekiq.configure_server do |config|
-    config.redis = { url: redis_url }
-  end
+redis_url = ENV['REDIS_URL'] || 'redis://localhost:6379'
+Sidekiq.configure_server do |config|
+  config.redis = { url: redis_url }
+end
 
-  Sidekiq.configure_client do |config|
-    config.redis = { url: redis_url }
-  end
+Sidekiq.configure_client do |config|
+  config.redis = { url: redis_url }
 end
