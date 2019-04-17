a03668c2fb3817896c999fa0340be8def5e5c17c
Guillermo Esteves <g@gesteves.com>
2019-04-16 19:49:39 -0600

    Refactor flickr groups

--- app/workers/flickr_group_worker.rb
+++ app/workers/flickr_group_worker.rb
@@ -1,17 +1,22 @@
 class FlickrGroupWorker < ApplicationWorker
   sidekiq_options queue: 'low'
 
-  def perform(photo_id, group_id)
+  def perform(photo_id, group_url)
     return if !Rails.env.production?
-    FlickRaw.api_key = ENV['flickr_consumer_key']
-    FlickRaw.shared_secret = ENV['flickr_consumer_secret']
-
-    flickr = FlickRaw::Flickr.new
-    flickr.access_token = ENV['flickr_access_token']
-    flickr.access_secret = ENV['flickr_access_token_secret']
-
     begin
-      flickr.groups.pools.add(photo_id: photo_id, group_id: group_id)
+      FlickRaw.api_key = ENV['flickr_consumer_key']
+      FlickRaw.shared_secret = ENV['flickr_consumer_secret']
+
+      flickr = FlickRaw::Flickr.new
+      flickr.access_token = ENV['flickr_access_token']
+      flickr.access_secret = ENV['flickr_access_token_secret']
+      slug = group_url.split('/').last
+      group = if /\d+@N\d+/.match? slug
+        flickr.groups.getInfo(group_id: slug)
+      else
+        flickr.groups.getInfo(group_path_alias: slug)
+      end
+      flickr.groups.pools.add(photo_id: photo_id, group_id: group['nsid'])
     rescue FlickRaw::FailedResponse
     end
   end
--- app/workers/flickr_worker.rb
+++ app/workers/flickr_worker.rb
@@ -5,29 +5,32 @@ class FlickrWorker < ApplicationWorker
     photo = Photo.find(photo_id)
     entry = photo.entry
     return if !entry.is_published? || !Rails.env.production?
-    FlickRaw.api_key = ENV['flickr_consumer_key']
-    FlickRaw.shared_secret = ENV['flickr_consumer_secret']
+    begin
+      FlickRaw.api_key = ENV['flickr_consumer_key']
+      FlickRaw.shared_secret = ENV['flickr_consumer_secret']
 
-    flickr = FlickRaw::Flickr.new
-    flickr.access_token = ENV['flickr_access_token']
-    flickr.access_secret = ENV['flickr_access_token_secret']
+      flickr = FlickRaw::Flickr.new
+      flickr.access_token = ENV['flickr_access_token']
+      flickr.access_secret = ENV['flickr_access_token_secret']
 
-    title = entry.title
+      title = entry.title
 
-    if entry.body.present?
-      body = "#{entry.formatted_body}\n\n#{entry.permalink_url}"
-    else
-      body = entry.permalink_url
-    end
-
-    all_tags = entry.combined_tag_list.map { |t| "\"#{t.gsub(/["']/, '')}\"" }.join(' ')
-
-    photo_id = flickr.upload_photo open(photo.image.service_url).path, title: title, description: body, tags: all_tags
-
-    if photo_id.present?
-      entry.flickr_groups.each do |group_id|
-        FlickrGroupWorker.perform_async(photo_id, group_id)
+      if entry.body.present?
+        body = "#{entry.formatted_body}\n\n#{entry.permalink_url}"
+      else
+        body = entry.permalink_url
       end
+
+      all_tags = entry.combined_tag_list.map { |t| "\"#{t.gsub(/["']/, '')}\"" }.join(' ')
+
+      photo_id = flickr.upload_photo open(photo.image.service_url).path, title: title, description: body, tags: all_tags
+
+      if photo_id.present?
+        entry.flickr_groups.each do |group_url|
+          FlickrGroupWorker.perform_async(photo_id, group_url)
+        end
+      end
+    rescue FlickRaw::FailedResponse
     end
   end
 end
