6eb0346484b13989e3e0f995a82672d935546b0a
Guillermo Esteves <g@gesteves.com>
2019-05-07 07:37:01 -0600

    Don't rescue flickr exceptions

--- app/workers/flickr_worker.rb
+++ app/workers/flickr_worker.rb
@@ -9,36 +9,32 @@ class FlickrWorker < ApplicationWorker
     end
     entry = photo.entry
     return if !entry.is_published? || !Rails.env.production?
-    begin
-      FlickRaw.api_key = ENV['flickr_consumer_key']
-      FlickRaw.shared_secret = ENV['flickr_consumer_secret']
+    FlickRaw.api_key = ENV['flickr_consumer_key']
+    FlickRaw.shared_secret = ENV['flickr_consumer_secret']
 
-      flickr = FlickRaw::Flickr.new
-      flickr.access_token = ENV['flickr_access_token']
-      flickr.access_secret = ENV['flickr_access_token_secret']
+    flickr = FlickRaw::Flickr.new
+    flickr.access_token = ENV['flickr_access_token']
+    flickr.access_secret = ENV['flickr_access_token_secret']
 
-      title = entry.title
+    title = entry.title
 
-      if entry.body.present?
-        body = "#{entry.formatted_body}\n\n#{entry.permalink_url}"
-      else
-        body = entry.permalink_url
+    if entry.body.present?
+      body = "#{entry.formatted_body}\n\n#{entry.permalink_url}"
+    else
+      body = entry.permalink_url
+    end
+
+    all_tags = entry.combined_tag_list.map { |t| "\"#{t.gsub(/["']/, '')}\"" }.join(' ')
+
+    photo_id = flickr.upload_photo open(photo.image.service_url).path, title: title, description: body, tags: all_tags
+
+    if photo_id.present?
+      entry.flickr_groups.each do |group_url|
+        FlickrGroupWorker.perform_async(photo_id, group_url)
       end
-
-      all_tags = entry.combined_tag_list.map { |t| "\"#{t.gsub(/["']/, '')}\"" }.join(' ')
-
-      photo_id = flickr.upload_photo open(photo.image.service_url).path, title: title, description: body, tags: all_tags
-
-      if photo_id.present?
-        entry.flickr_groups.each do |group_url|
-          FlickrGroupWorker.perform_async(photo_id, group_url)
-        end
-        entry.flickr_albums.each do |album_url|
-          FlickrAlbumWorker.perform_async(photo_id, album_url)
-        end
+      entry.flickr_albums.each do |album_url|
+        FlickrAlbumWorker.perform_async(photo_id, album_url)
       end
-    rescue FlickRaw::FailedResponse => e
-      logger.error "[Flickr] [Photo] [#{entry.permalink_url}] #{e}"
     end
   end
 end
