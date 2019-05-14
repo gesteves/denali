722d7ae943d9858c7e82f00c1790b9431f5dcbcd
Guillermo Esteves <g@gesteves.com>
2019-05-14 06:17:46 -0600

    Only check updates with comments enabled

--- app/workers/instagram_comment_worker.rb
+++ app/workers/instagram_comment_worker.rb
@@ -5,16 +5,18 @@ class InstagramCommentWorker < ApplicationWorker
     return if ENV['buffer_access_token'].blank?
     response = HTTParty.get("https://api.bufferapp.com/1/updates/#{update_id}.json?access_token=#{ENV['buffer_access_token']}")
     update = JSON.parse(response.body)
-    if update['comment_enabled'] && update.dig('comment_state', 'success')
-      logger.info "[Instagram] Comment posted for update #{update_id}: #{update['comment_text']}"
-    else
-      logger.error "[Instagram] Comment failed for update #{update_id}: #{update.dig('comment_state', 'error')}"
-      payload = {
-        value1: update['text'],
-        value2: update['service_link'],
-        value3: update['media']['picture']
-      }
-      IftttWebhookWorker.perform_async('instagram-comment-failed', payload)
+    if update['comment_enabled']
+      if update.dig('comment_state', 'success')
+        logger.info "[Instagram] Comment posted for update #{update_id}: #{update['comment_text']}"
+      elsif update.dig('comment_state', 'error').present?
+        logger.error "[Instagram] Comment failed for update #{update_id}: #{update.dig('comment_state', 'error')}"
+        payload = {
+          value1: update['text'],
+          value2: update['service_link'],
+          value3: update['media']['picture']
+        }
+        IftttWebhookWorker.perform_async('instagram-comment-failed', payload)
+      end
     end
   end
 end
