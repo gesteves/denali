d02f8fd86ffa48f75c0378e85455b10fc9426352
Guillermo Esteves <g@gesteves.com>
2019-06-09 15:59:23 -0600

    Add search field

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -8,6 +8,11 @@ module Types
       argument :page, Integer, default_value: 1, required: false
       argument :count, Integer, default_value: 10, required: false, prepare: -> (count, ctx) { [count, 100].min }
     end
+    field :search, [Types::EntryType], null: true do
+      argument :term, String, required: true
+      argument :page, Integer, default_value: 1, required: false
+      argument :count, Integer, default_value: 10, required: false, prepare: -> (count, ctx) { [count, 100].min }
+    end
 
     def blog
       Blog.first
@@ -21,6 +26,12 @@ module Types
 
     def entries(page:, count:)
       Entry.published.page(page).per(count)
+    end
+
+    def search(term:, page:, count:)
+      results = Entry.published_search(term, page, count)
+      total_count = results.results.total
+      Kaminari.paginate_array(results.records, total_count: total_count).page(page).per(count)
     end
   end
 end
