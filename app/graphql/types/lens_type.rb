45d742b59628653dda14fe105171e3fce1e96119
Guillermo Esteves <g@gesteves.com>
2019-06-08 21:48:53 -0600

    Change field name

--- app/graphql/types/lens_type.rb
+++ app/graphql/types/lens_type.rb
@@ -3,6 +3,6 @@ module Types
     field :id, ID, null: false
     field :make, String, null: true
     field :model, String, null: true
-    field :display_name, String, null: true
+    field :name, String, null: true, method: :display_name
   end
 end
