0ad25fc15a87ba29d4607b2dc31894ec0c515657
Guillermo Esteves <g@gesteves.com>
2019-07-30 14:56:40 -0600

    Update buffer_worker.rb

--- app/workers/buffer_worker.rb
+++ app/workers/buffer_worker.rb
@@ -6,13 +6,11 @@ class BufferWorker < ApplicationWorker
   def get_profile_ids(service)
     return if ENV['buffer_access_token'].blank?
     response = HTTParty.get("https://api.bufferapp.com/1/profiles.json?access_token=#{ENV['buffer_access_token']}")
-    response = JSON.parse(response.body)
-    if response['success']
-      response.select { |profile| profile['service'].downcase.match(service) }.map { |profile| profile['id'] }
+    if response.code == 200
+      profiles = JSON.parse(response.body)
+      profiles.select { |profile| profile['service'].downcase.match(service) }.map { |profile| profile['id'] }
     else
-      code = response['code']
-      message = response['message']
-      raise "#{code} #{message}"
+      raise "#{response.code} #{response.body}"
     end
   end
 
