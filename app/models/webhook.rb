744620b4bb6ae8b234a0bfe1387d31be3694e10f
Guillermo Esteves <g@gesteves.com>
2019-08-05 20:26:19 -0600

    Use blocks

--- app/models/webhook.rb
+++ app/models/webhook.rb
@@ -1,4 +1,6 @@
 class Webhook < ApplicationRecord
+  include ActionView::Helpers::TextHelper
+
   belongs_to :blog
 
   validates :url, presence: true
@@ -37,18 +39,12 @@ class Webhook < ApplicationRecord
   end
 
   def to_slack(entry)
-    attachment = {
-      fallback: "#{entry.plain_title} #{entry.short_permalink_url}",
-      title: entry.plain_title,
-      title_link: entry.permalink_url,
-      text: entry.plain_body,
-      author_name: "#{entry.user.first_name} #{entry.user.last_name}",
-      author_icon: entry.user.avatar_url,
-      ts: entry.published_at.to_i
-    }
-    attachment[:image_url] = entry.photos.first.url(w: 1200) if entry.is_photo?
-    attachment[:color] = '#BF0222'
-    { text: '', attachments: [attachment] }.to_json
+    blocks = []
+    blocks << { type: 'section', text: { type: 'mrkdwn', text: "<#{entry.permalink_url}|*#{entry.plain_title}*>" } }
+    blocks << { type: 'section', text: { type: 'mrkdwn', text: truncate(entry.plain_body, length: 200) } } if entry.body.present?
+    blocks << { type: 'image', image_url: entry.photos.first.url(w: 1200), alt_text: entry.photos.first.alt_text } if entry.is_photo?
+
+    { text: "#{entry.plain_title} #{entry.short_permalink_url}", blocks: blocks }.to_json
   end
 
   def cleanup_url
