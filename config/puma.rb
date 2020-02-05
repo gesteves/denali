29a3498ddcb30fc29ebe56832e4de489bd8b7650
Guillermo Esteves <g@gesteves.com>
2020-02-05 06:55:03 -0700

    Disable puma_worker_killer

--- config/puma.rb
+++ config/puma.rb
@@ -33,11 +33,11 @@ preload_app!
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
 
-before_fork do
-  require 'puma_worker_killer'
+# before_fork do
+#   require 'puma_worker_killer'
 
-  PumaWorkerKiller.enable_rolling_restart # Default is every 6 hours
-end
+#   PumaWorkerKiller.enable_rolling_restart # Default is every 6 hours
+# end
 
 on_worker_boot do
   Barnes.start
