4f2782175642ca28db20ff13765c7b0fb5246c18
Guillermo Esteves <g@gesteves.com>
2020-02-13 13:42:15 -0700

    Logging tweak

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -125,8 +125,8 @@ class ApplicationController < ActionController::Base
 
   def log_conditional_get
     if is_cloudfront?
-      logger.info "[conditional get] If-None-Match header present? #{request.headers['If-None-Match'].present?}"
-      logger.info "[conditional get] If-Modified-Since header present? #{request.headers['If-Modified-Since'].present?}"
+      logger.info "[conditional get] If-None-Match header: #{request.headers['If-None-Match']}"
+      logger.info "[conditional get] If-Modified-Since header: #{request.headers['If-Modified-Since']}"
     end
   end
 end
