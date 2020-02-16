ae10beb84b90e3587dd2f950795bad943d4a8b57
Guillermo Esteves <g@gesteves.com>
2020-02-16 10:43:54 -0700

    Add unmatched route

--- config/routes.rb
+++ config/routes.rb
@@ -109,5 +109,6 @@ Rails.application.routes.draw do
   match '/404', to: 'errors#file_not_found', via: :all
   match '/422', to: 'errors#unprocessable', via: :all
   match '/500', to: 'errors#internal_server_error', via: :all
+  get '*unmatched_route', to: 'errors#file_not_found'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
