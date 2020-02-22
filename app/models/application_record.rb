498ed9ce8bc64a603857de97929be003df5eabc8
Guillermo Esteves <g@gesteves.com>
2020-02-22 15:36:41 -0700

    Revert "Use a separate env var"
    This reverts commit 461a61905e67d718b34028dd20724f92a9fb78c4.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,16 +2,16 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   def cache_key
-    if cache_key_version = ENV['CACHE_KEY_VERSION']
-      "#{cache_key_version}/#{super}"
+    if heroku_release_version = ENV['HEROKU_RELEASE_VERSION']
+      "#{heroku_release_version}/#{super}"
     else
       super
     end
   end
 
   def self.collection_cache_key(collection, timestamp_column)
-    if cache_key_version = ENV['CACHE_KEY_VERSION']
-      "#{cache_key_version}/#{super}"
+    if heroku_release_version = ENV['HEROKU_RELEASE_VERSION']
+      "#{heroku_release_version}/#{super}"
     else
       super
     end
