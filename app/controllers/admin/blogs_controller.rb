9b85b094bc8a33545b7a03817094d9d05c4598e6
Guillermo Esteves <g@gesteves.com>
2020-02-22 10:24:56 -0700

    Revert "Add analytics field to blogs"
    This reverts commit ff4bab354e632c0f962ceff384d41ea753d76135.

--- app/controllers/admin/blogs_controller.rb
+++ app/controllers/admin/blogs_controller.rb
@@ -34,7 +34,7 @@ class Admin::BlogsController < AdminController
 
   def blog_params
     params.require(:blog).permit(:name, :tag_line, :posts_per_page, :about, :copyright,
-                                 :show_related_entries, :analytics,
+                                 :show_related_entries, :analytics_code,
                                  :instagram, :twitter, :tumblr, :email, :flickr,
                                  :header_logo_svg, :additional_meta_tags,
                                  :favicon, :touch_icon, :logo, :facebook, :time_zone, :meta_description, :map_style)
--- db/migrate/20200222171801_add_analytic_to_blogs.rb
+++
@@ -1,5 +0,0 @@
-class AddAnalyticToBlogs < ActiveRecord::Migration[5.2]
-  def change
-    add_column :blogs, :analytics, :text
-  end
-end
