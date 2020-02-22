9a8476f93366b1477901e0b63a9a820633d84360
Guillermo Esteves <g@gesteves.com>
2020-02-22 15:21:38 -0700

    Don't skip filter

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,6 @@
 class SessionsController < ApplicationController
   before_action :block_cloudfront
   before_action :no_cache
-  skip_before_action :domain_redirect
 
   def new
     @page_title = "Sign in · #{@photoblog.name}"
