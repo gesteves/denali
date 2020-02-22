219bf1a861d67e045964538ef3be66054e2fdf0b
Guillermo Esteves <g@gesteves.com>
2020-02-22 15:32:59 -0700

    Revert changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
   before_action :block_cloudfront
   before_action :no_cache
+  skip_before_action :domain_redirect
 
   def new
     @page_title = "Sign in · #{@photoblog.name}"
