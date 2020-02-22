461a61905e67d718b34028dd20724f92a9fb78c4
Guillermo Esteves <g@gesteves.com>
2020-02-22 15:33:46 -0700

    Use a separate env var

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,16 +2,16 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   def cache_key
-    if heroku_release_version = ENV['HEROKU_RELEASE_VERSION']
-      "#{heroku_release_version}/#{super}"
+    if cache_key_version = ENV['CACHE_KEY_VERSION']
+      "#{cache_key_version}/#{super}"
     else
       super
     end
   end
 
   def self.collection_cache_key(collection, timestamp_column)
-    if heroku_release_version = ENV['HEROKU_RELEASE_VERSION']
-      "#{heroku_release_version}/#{super}"
+    if cache_key_version = ENV['CACHE_KEY_VERSION']
+      "#{cache_key_version}/#{super}"
     else
       super
     end
