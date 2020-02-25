119e9e59f20baee212f4ec5a95ff311d43d3b223
Guillermo Esteves <g@gesteves.com>
2020-02-25 06:41:28 -0700

    Update legacy_controller.rb

--- app/controllers/legacy_controller.rb
+++ app/controllers/legacy_controller.rb
@@ -1,13 +1,13 @@
 class LegacyController < ApplicationController
   skip_before_action :verify_authenticity_token
 
-  def archive
+  def home
     http_cache_forever(public: true) do
       redirect_to(root_url, status: 301)
     end
   end
 
-  def rss
+  def feed
     http_cache_forever(public: true) do
       redirect_to(feed_url, status: 301)
     end
