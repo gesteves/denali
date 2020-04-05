4d4628cdc27d100dfec0e3892d9dc46aec3b318b
Guillermo Esteves <g@gesteves.com>
2020-04-05 12:39:19 -0600

    Changes to feeds

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -16,4 +16,18 @@ module EntriesHelper
       photo.url(w: 1200, square: true)
     ]
   end
+
+  def exif_for_feeds(photo, separator: ' · ')
+    exif = []
+    exif << "#{photo.focal_length_with_unit} focal length" if photo.focal_length.present?
+    if photo.exposure.present? && photo.f_number.present?
+      exif << "#{photo.formatted_exposure} at #{photo.formatted_aperture}"
+    elsif photo.exposure.present?
+      exif << photo.formatted_aperture
+    elsif photo.f_number.present?
+      exif << photo.formatted_aperture
+    end
+    exif << "ISO #{photo.iso}"
+    exif.join(separator)
+  end
 end
