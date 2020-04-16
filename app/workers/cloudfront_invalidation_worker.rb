9693e272a79c839d6ed27db4239a6d3a2fb480d0
Guillermo Esteves <g@gesteves.com>
2020-04-16 09:25:20 -0600

    Fix worker

--- app/workers/cloudfront_invalidation_worker.rb
+++ app/workers/cloudfront_invalidation_worker.rb
@@ -1,6 +1,6 @@
 class CloudfrontInvalidationWorker < ApplicationWorker
   def perform(paths = ['/*'])
-    paths = [paths].flatten.uniq.reject(&:blank?).reject { |path| path.start_with? '/' }
+    paths = [paths].flatten.uniq.reject(&:blank?).reject { |path| !path.start_with? '/' }
     return if ENV['CACHE_TTL'].to_i <= 60
     return if paths.blank?
     return if !Rails.env.production?
