e0e1690a5b6dd8edd7e38f86074cea988ba38433
Guillermo Esteves <g@gesteves.com>
2020-04-20 09:57:45 -0600

    Update twitter_worker.rb

--- app/workers/twitter_worker.rb
+++ app/workers/twitter_worker.rb
@@ -6,17 +6,6 @@ class TwitterWorker < BufferWorker
     caption = entry.tweet_text.present? ? entry.tweet_text : entry.plain_title
     text = "#{truncate(caption.gsub(/\s+&\s+/, ' and '), length: max_length, omission: '…')} #{entry.permalink_url}"
     now = Rails.env.production? ? now : false
-    if entry.is_photo?
-      photos = entry.photos.to_a[0..4]
-      opts = {
-        text: text,
-        media: media_hash(photos.shift, alt_text: true),
-        now: now
-      }
-      opts[:extra_media] = photos.map { |p| media_hash(p, alt_text: true) } if photos.present?
-      post_to_buffer('twitter', opts)
-    else
-      post_to_buffer('twitter', text: text, now: now)
-    end
+    post_to_buffer('twitter', text: text, now: now)
   end
 end
