19ef7e0906f6e2d1b230b53be76ed795c59d5857
Guillermo Esteves <g@gesteves.com>
2020-04-19 18:49:38 -0600

    Don't check config

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,7 +2,7 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   def cache_key
-    cache_version = Rails.configuration.action_controller.perform_caching ? ENV['CACHE_VERSION'] : Time.now.to_i
+    cache_version = ENV['CACHE_VERSION']
     if cache_version.present?
       "#{cache_version}/#{super}"
     else
@@ -11,7 +11,7 @@ class ApplicationRecord < ActiveRecord::Base
   end
 
   def self.collection_cache_key(collection, timestamp_column)
-    cache_version = Rails.configuration.action_controller.perform_caching ? ENV['CACHE_VERSION'] : Time.now.to_i
+    cache_version = ENV['CACHE_VERSION']
     if cache_version.present?
       "#{cache_version}/#{super}"
     else
