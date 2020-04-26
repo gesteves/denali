dbdc5bdf2cf0465b9d86bf235485f58112a34a2c
Guillermo Esteves <g@gesteves.com>
2020-04-26 09:47:17 -0600

    Remove dependent

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :entries, dependent: :destroy
+  has_many :entries
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
