5b3449925518cc267f8ae7c796720852547b2916
Guillermo Esteves <g@gesteves.com>
2020-05-10 18:25:36 -0600

    Remove some query types

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -13,18 +13,6 @@ module Types
       argument :page, Integer, default_value: 1, required: false
       argument :count, Integer, default_value: 10, required: false, prepare: -> (count, ctx) { [count, 100].min }
     end
-    field :cameras, [Types::CameraType], null: true do
-      argument :page, Integer, default_value: 1, required: false
-      argument :count, Integer, default_value: 10, required: false, prepare: -> (count, ctx) { [count, 100].min }
-    end
-    field :lenses, [Types::LensType], null: true do
-      argument :page, Integer, default_value: 1, required: false
-      argument :count, Integer, default_value: 10, required: false, prepare: -> (count, ctx) { [count, 100].min }
-    end
-    field :films, [Types::FilmType], null: true do
-      argument :page, Integer, default_value: 1, required: false
-      argument :count, Integer, default_value: 10, required: false, prepare: -> (count, ctx) { [count, 100].min }
-    end
 
     def blog
       Blog.first
@@ -44,18 +32,6 @@ module Types
       results = Entry.published_search(term, page, count)
       total_count = results.results.total
       Kaminari.paginate_array(results.records, total_count: total_count).page(page).per(count)
-    end
-
-    def cameras(page:, count:)
-      Camera.all.page(page).per(count)
-    end
-
-    def lenses(page:, count:)
-      Lens.all.page(page).per(count)
-    end
-
-    def films(page:, count:)
-      Film.all.page(page).per(count)
     end
   end
 end
