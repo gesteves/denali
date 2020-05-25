a86fea62728c2ceb3d366d7329881ebb9cb624ca
Guillermo Esteves <g@gesteves.com>
2020-05-25 16:53:36 -0600

    Add resize observer

--- app/frontend/controllers/application/grid_controller.js
+++ app/frontend/controllers/application/grid_controller.js
@@ -27,13 +27,23 @@ export default class extends Controller {
       }
     });
 
-    setTimeout(() => this.masonry.layout(), 10);
+    this.masonry.layout();
 
     // Set up a mutation observer to observe the grid container for any
     // changes in the `childList`, so that if any nodes get added to the container,
     // they get placed in the Masonry layout.
-    this.observer = new MutationObserver(e => this.handleMutations(e));
-    this.observer.observe(this.element, { childList: true });
+    this.mutationObserver = new MutationObserver(e => this.handleMutations(e));
+    this.mutationObserver.observe(this.element, { childList: true });
+
+    // Set up a resize observer to work around a weird race condition in Safari,
+    // which causes the masonry to be set up (incorrectly) before styles are applied.
+    // When the styles load, the element should resize and call `masonry.layout()` again.
+    if ('ResizeObserver' in window) {
+      this.resizeObserver = new ResizeObserver(() => {
+        this.masonry.layout();
+      });
+      this.resizeObserver.observe(this.element);
+    }
   }
 
   /**
