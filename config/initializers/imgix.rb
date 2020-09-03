6c14e11168ae865d998006490be809e93359a746
Guillermo Esteves <g@gesteves.com>
2020-09-03 16:57:03 -0600

    Swap host for domain

--- config/initializers/imgix.rb
+++ config/initializers/imgix.rb
@@ -1,1 +1,1 @@
-::Ix = Imgix::Client.new(host: ENV['imgix_domain'], secure_url_token: ENV['imgix_token'], include_library_param: false, use_https: true, api_key: ENV['imgix_api_key'])
+::Ix = Imgix::Client.new(domain: ENV['imgix_domain'], secure_url_token: ENV['imgix_token'], include_library_param: false, use_https: true, api_key: ENV['imgix_api_key'])
