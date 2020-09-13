25e17252026847558a393418a6efe6c9bff4c124
Guillermo Esteves <g@gesteves.com>
2020-09-13 08:46:41 -0600

    Fix test

--- test/controllers/oembed_controller_test.rb
+++ test/controllers/oembed_controller_test.rb
@@ -2,13 +2,13 @@ require 'test_helper'
 
 class OembedControllerTest < ActionController::TestCase
   test "oembed should work for published entries" do
-    get :show, params: { url: entries(:peppers).permalink_url }
+    get :show, params: { url: entries(:peppers).permalink_url, format: 'json' }
     assert_response :success
     assert_template :show
   end
 
   test "oembed should work for non-published entries" do
-    get :show, params: { url: entries(:panda).permalink_url }
+    get :show, params: { url: entries(:panda).permalink_url, format: 'json' }
     assert_response :success
     assert_template :show
   end
