cacb3787ab54e37719d1081c63e212771f2b5516
Guillermo Esteves <g@gesteves.com>
2020-09-13 16:08:19 -0600

    Ignore exception

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -2,5 +2,6 @@ if ENV['sentry_dsn'].present?
   Raven.configure do |config|
     config.dsn = ENV['sentry_dsn']
     config.environments = ['staging', 'production']
+    config.excluded_exceptions += ['PhotoNotUploadedError']
   end
 end
