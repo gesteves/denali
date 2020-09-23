fb97a8d20a7eaabf20752a69ecaadaccead5de54
Guillermo Esteves <guillermo@voxmedia.com>
2020-09-23 12:13:21 -0600

    Retry unprocessed photos faster

--- app/workers/application_worker.rb
+++ app/workers/application_worker.rb
@@ -2,4 +2,11 @@ class ApplicationWorker
   include Sidekiq::Worker
   include ActionView::Helpers::TextHelper
   sidekiq_options queue: 'default'
+
+  sidekiq_retry_in do |count, exception|
+    case exception
+    when UnprocessedPhotoError
+      count
+    end
+  end
 end
