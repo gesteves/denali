0dac38fbaafb813be796152d5ec8f95a3238407b
Guillermo Esteves <g@gesteves.com>
2020-12-05 17:05:07 -0700

    Fix map

--- app/frontend/controllers/admin/map_controller.js
+++ app/frontend/controllers/admin/map_controller.js
@@ -11,7 +11,7 @@ import { Controller }             from 'stimulus';
 export default class extends Controller {
   static targets = ['container', 'spinner'];
   static values = {
-    style: String,
+    mapStyle: String,
     apiToken: String,
     markersUrl: String
   }
