851c947aa6663ddb753b3380dbaac21392fd40b9
Guillermo Esteves <g@gesteves.com>
2021-01-09 20:22:38 -0700

    Change cache key

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,10 +2,10 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   def cache_key
-    "#{ENV['HEROKU_RELEASE_VERSION']}/#{ENV['CACHE_VERSION']}/#{super}"
+    "#{ENV['CACHE_VERSION']}/#{super}"
   end
 
   def self.collection_cache_key(collection, timestamp_column)
-    "#{ENV['HEROKU_RELEASE_VERSION']}/#{ENV['CACHE_VERSION']}/#{super}"
+    "#{ENV['CACHE_VERSION']}/#{super}"
   end
 end
