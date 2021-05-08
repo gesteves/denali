126ffe1d52cc433a087c8db7280b6c392b961a8d
Guillermo Esteves <g@gesteves.com>
2021-05-08 17:23:14 -0600

    Use setinterval

--- app/frontend/controllers/shared/blurhash_controller.js
+++ app/frontend/controllers/shared/blurhash_controller.js
@@ -2,30 +2,27 @@ import { Controller } from 'stimulus';
 
 /**
  * Removes Blurhash backgrounds after the photos load.
- * Uses RAF to try to ensure the background only gets removed
- * after the real image is rendered, to avoid annoying flashes.
  * @extends Controller
  */
 export default class extends Controller {
 
   connect () {
     if (this.element.complete && this.element.naturalWidth > 0 && this.element.naturalHeight > 0) {
-      requestAnimationFrame(() => this.removeBackground());
+      this.removeBackground();
     }
   }
 
   /**
    * Removes the backgrounds when photos load.
+   * Uses a setInterval to give the real photo a chance to render
+   * before removing the background, to prevent annoying flashing.
    */
   removeBackground () {
-    requestAnimationFrame(() => this.element.classList.remove('blurhash'));
-  }
-
-  imageLoaded () {
-    requestAnimationFrame(() => this.startRender());
-  }
-
-  startRender () {
-    requestAnimationFrame(() => this.removeBackground());
+    const interval = setInterval(() => {
+      if (this.element.naturalWidth > 0 && this.element.naturalHeight > 0) {
+        clearInterval(interval);
+        requestAnimationFrame(() => this.element.classList.remove('blurhash'));
+      }
+    }, 1000);
   }
 }
