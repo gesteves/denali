d6dbea0c9c20f702652970edafefc799dbc3636e
Guillermo Esteves <g@gesteves.com>
2021-05-08 08:47:50 -0600

    Remove blurhash js controller

--- app/frontend/packs/admin.js
+++ app/frontend/packs/admin.js
@@ -6,6 +6,9 @@ import Turbolinks from 'turbolinks';
 
 const application = Application.start();
 const context = require.context('controllers/admin', true, /.js$/);
+const sharedContext = require.context('controllers/shared', true, /.js$/);
 application.load(definitionsFromContext(context));
+application.load(definitionsFromContext(sharedContext));
+
 Rails.start();
 Turbolinks.start();
