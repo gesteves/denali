880990941dfd6823882a738397afaa415b322528
Guillermo Esteves <g@gesteves.com>
2021-05-08 17:33:51 -0600

    Tweaks

--- app/frontend/controllers/shared/blurhash_controller.js
+++ app/frontend/controllers/shared/blurhash_controller.js
@@ -7,7 +7,7 @@ import { Controller } from 'stimulus';
 export default class extends Controller {
 
   connect () {
-    if (this.element.complete && this.element.naturalWidth > 0 && this.element.naturalHeight > 0) {
+    if (this.element.complete) {
       this.removeBackground();
     }
   }
