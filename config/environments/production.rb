5a17a6bfd2b5f0d473c8bed2d55e0529e5e6d9a3
Guillermo Esteves <g@gesteves.com>
2021-05-15 12:10:52 -0600

    Add comment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
+  # Needs to be enabled to allow inline assets.
   config.assets.compile = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
