95d4df8672f1cde0b6e1c71396d79ea2d6f40404
Guillermo Esteves <g@gesteves.com>
2021-10-10 18:39:42 -0600

    Refactor

--- app/workers/google_vision_worker.rb
+++ app/workers/google_vision_worker.rb
@@ -7,12 +7,11 @@ class GoogleVisionWorker < ApplicationWorker
     raise UnprocessedPhotoError unless photo.processed?
 
     response = request_annotations(photo)
-    response_colors = response['responses']&.first&.dig('imagePropertiesAnnotation', 'dominantColors', 'colors')
-    raise NoColorsError if response_colors.blank?
+    colors = response['responses'].find { |r| r['imagePropertiesAnnotation'].present? }.dig('imagePropertiesAnnotation', 'dominantColors', 'colors')
 
-    photo.dominant_color = dominant_color(response_colors)
-    photo.black_and_white = is_black_and_white?(response_colors)
-    photo.color = !is_black_and_white?(response_colors)
+    photo.dominant_color = dominant_color(colors)
+    photo.black_and_white = is_black_and_white?(colors)
+    photo.color = !is_black_and_white?(colors)
     photo.save
   end
 
@@ -39,7 +38,8 @@ class GoogleVisionWorker < ApplicationWorker
 
     json = JSON.parse(response.body)
     raise GoogleVisionAccessError if json['responses'].any? { |r| r.dig('error', 'code').to_i == 4 }
-    raise json['responses'].find { |r| r['error'].present? }.dig('error', 'message') if json['responses'].any? { |r| r['error'].present? }
+    raise NoColorsError if response['responses'].none? { |r| r['imagePropertiesAnnotation'].present? }
+    raise "#{json['responses'].find { |r| r['error'].present? }.dig('error', 'message')} (Error code: #{json['responses'].find { |r| r['error'].present? }.dig('error', 'code')})" if json['responses'].any? { |r| r['error'].present? }
     json
   end
 
