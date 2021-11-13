423c73607045531ea28ad89d20d911074e8356da
Guillermo Esteves <g@gesteves.com>
2021-11-13 11:01:10 -0700

    Use p for short URLs

--- config/routes.rb
+++ config/routes.rb
@@ -78,7 +78,7 @@ Rails.application.routes.draw do
   get '/search'                        => 'entries#search', :as => :search
 
   # Entry
-  get '/e/:id'                              => 'entries#short',        constraints: { id: /\w+/ }, :as => :entry
+  get '/p/:id'                              => 'entries#short',       constraints: { id: /\w+/ }, :as => :entry
   get '/preview/:preview_hash'              => 'entries#show',        defaults: { format: 'html' }, :as => :preview_entry
   get '/:id(/:slug)'                        => 'entries#show',        constraints: { id: /\d+/ }, defaults: { format: 'html' }, :as => :entry_long
   get '/related/:id.:format'                => 'entries#related',     defaults: { format: 'js' }, constraints: { id: /\d+/ }, :as => :related
