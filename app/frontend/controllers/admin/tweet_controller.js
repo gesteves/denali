0cf9afac512b3f7c6541401ce2a677598a5fc6d6
Guillermo Esteves <g@gesteves.com>
2021-11-24 12:13:27 -0700

    Add tweet count controller

---
+++ app/frontend/controllers/admin/tweet_controller.js
@@ -0,0 +1,26 @@
+import { Controller } from 'stimulus';
+
+/**
+ * Controls copy-to-clipboard functionality.
+ * @extends Controller
+ */
+export default class extends Controller {
+  static targets = ['characterCount', 'input'];
+
+  connect () {
+    this.updateCharacterCount();
+  }
+
+  /**
+   * Count characters in the input and updates the character count.
+   */
+  updateCharacterCount () {
+    var count = this.inputTarget.value.length;
+    this.characterCountTarget.innerHTML = count;
+    if (count > 220) {
+      this.characterCountTarget.classList.add('has-text-danger');
+    } else {
+      this.characterCountTarget.classList.remove('has-text-danger');
+    }
+  }
+}
