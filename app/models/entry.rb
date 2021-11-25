420f5b9b069f9735a4dd1fdd9f3c1edb5d5e6552
Guillermo Esteves <g@gesteves.com>
2021-11-25 11:54:04 -0700

    Update how slugs are generated

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -574,7 +574,7 @@ class Entry < ApplicationRecord
 
   def set_entry_slug
     if self.slug.blank?
-      self.slug = self.title.split('.').first.parameterize
+      self.slug = self.title.split(/[,.]/).first.parameterize
     else
       self.slug = self.slug.parameterize
     end
