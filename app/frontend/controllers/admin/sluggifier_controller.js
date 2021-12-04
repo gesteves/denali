0474e14a9e47d3539dca9d9cb40bc7ea8a32e8fe
Guillermo Esteves <g@gesteves.com>
2021-12-03 17:52:59 -0700

    Update comments

--- app/frontend/controllers/admin/sluggifier_controller.js
+++ app/frontend/controllers/admin/sluggifier_controller.js
@@ -12,7 +12,7 @@ export default class extends Controller {
   }
 
   /**
-   * Count characters in the input and updates the character count.
+   * Updates the slug with the parameterized version of the title, if applicable.
    */
   handleTitleChange () {
     if (!this.isSlugEditable || this.titleTarget.value.trim() === '') {
@@ -22,11 +22,22 @@ export default class extends Controller {
     this.slugTarget.value = this.parameterize(this.titleTarget.value);
   }
 
+  /**
+   * Determines if the slug should be modified by the title;
+   * it only becomes updatable by the title if it's empty.
+   * (This is so updating the title doesn't overwrite a manually-written slug.)
+   */
   handleSlugChange () {
     this.isSlugEditable = this.slugTarget.value.trim() === '';
     this.handleTitleChange();
   }
 
+  /**
+   * Half-assed port of Rails `parameterize` method
+   * https://www.rubydoc.info/gems/activesupport/5.0.0.1/ActiveSupport/Inflector#parameterize-instance_method
+   * @param {A string} string
+   * @returns string
+   */
   parameterize (string) {
     const separator = '-';
     const duplicateSeparatorRegex = /-{2,}/g;
