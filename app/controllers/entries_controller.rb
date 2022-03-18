742555bb8af9e42806db3a6642b6d2b854e1ba00
Guillermo Esteves <g@gesteves.com>
2022-03-17 20:39:07 -0600

    Only recentish photos in random

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -127,7 +127,7 @@ class EntriesController < ApplicationController
   end
 
   def random
-    entry = Entry.find(Entry.published.pluck(:id).sample)
+    entry = Entry.find(Entry.published.where('published_at >= ?', 3.years.ago).pluck(:id).sample)
     response.headers['Cache-Control'] = "s-maxage=1, max-age=0, public"
     redirect_to entry.permalink_url, status: 302
   end
