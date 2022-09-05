6831c645d6cf9288fc62b428338df7686b8dc2dd
Guillermo Esteves <g@gesteves.com>
2022-09-05 08:07:05 -0600

    [skip deploy] Support Heroku preview apps

--- config/initializers/default_url_options.rb
+++ config/initializers/default_url_options.rb
@@ -1,6 +1,8 @@
 if Rails.env.production?
   host = if ENV['RENDER'] && ENV['IS_PULL_REQUEST'] == 'true'
     ENV['RENDER_EXTERNAL_HOSTNAME']
+  elsif ENV['HEROKU_PARENT_APP_NAME'].present?
+    "#{ENV['HEROKU_APP_NAME']}.herokuapp.com"
   else
     ENV['DOMAIN']
   end
