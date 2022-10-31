3753931f9decb305fea7761a0b81e3c66b84269c
Guillermo Esteves <g@gesteves.com>
2022-10-31 15:42:09 -0600

    Attempt to update images

--- app/workers/tumblr_update_worker.rb
+++ app/workers/tumblr_update_worker.rb
@@ -15,22 +15,23 @@ class TumblrUpdateWorker < ApplicationWorker
     response = tumblr.posts(ENV['TUMBLR_DOMAIN'], id: tumblr_id)
     raise response.to_s if response['errors'].present? || (response['status'].present? && response['status'] >= 400)
 
-    post_format = response['posts'][0]['format']
-    link_url = response['posts'][0]['link_url']
-    is_published = response['posts'][0]['state'] == 'published'
+    tumblr_post_format = response['posts'][0]['format']
+    tumblr_post_type = response['posts']['0']['type']
+    is_published_on_tumblr = response['posts'][0]['state'] == 'published'
     use_html = post_format == 'html'
 
     opts = {
       id: tumblr_id,
+      type: tumblr_post_type.to_sym,
       tags: entry.tumblr_tags,
       slug: entry.slug,
       caption: entry.tumblr_caption(html: use_html),
       source_url: entry.permalink_url,
-      format: post_format
+      format: tumblr_post_format
     }
 
-    opts[:date] = entry.published_at.to_s if is_published
-    opts[:link] = '' if link_url.present?
+    opts[:date] = entry.published_at.to_s if is_published_on_tumblr
+    opts[:data] = entry.photos.map { |p| URI.open(p.url(w: 2048)).path } if entry.is_photo?
 
     response = tumblr.edit(ENV['TUMBLR_DOMAIN'], opts)
     raise response.to_s if response['errors'].present? || (response['status'].present? && response['status'] >= 400)
