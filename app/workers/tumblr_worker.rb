80bb4f809a3205d7ec92bccde13318dd6a00ea56
Guillermo Esteves <g@gesteves.com>
2022-11-12 07:53:21 -0700

    Add date

--- app/workers/tumblr_worker.rb
+++ app/workers/tumblr_worker.rb
@@ -18,7 +18,7 @@ class TumblrWorker < ApplicationWorker
       oauth_token: ENV['TUMBLR_ACCESS_TOKEN'],
       oauth_token_secret: ENV['TUMBLR_ACCESS_TOKEN_SECRET']
     })
-    
+
     opts = {
       tags: entry.tumblr_tags,
       slug: entry.slug,
@@ -26,9 +26,10 @@ class TumblrWorker < ApplicationWorker
       source_url: entry.permalink_url,
       format: 'markdown',
       state: 'queue',
+      date: entry.published_at.to_s,
       data: entry&.photos&.map { |p| URI.open(p.url(w: 2048)).path }
     }.compact
-    
+
     response = tumblr.photo(tumblr_username, opts)
 
     raise response.to_s if response['errors'].present? || (response['status'].present? && response['status'] >= 400)
