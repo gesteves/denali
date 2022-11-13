00664207001d6145e814d548546c9f493134f007
Guillermo Esteves <g@gesteves.com>
2022-11-13 15:32:44 -0700

    Fix

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -2,3 +2,4 @@ Bugsnag.configure do |config|
   config.api_key = ENV['BUGSNAG_API_KEY']
   config.app_version = ENV['HEROKU_RELEASE_VERSION'] || ENV['RENDER_GIT_COMMIT'] || ENV['FLY_ALLOC_ID']
   config.discard_classes += ['UnprocessedPhotoError', 'Aws::CloudFront::Errors::TooManyInvalidationsInProgress']
+end
