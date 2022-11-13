58f91a7b16f9ffdf327ae098c0ea41beac54e61e
Guillermo Esteves <g@gesteves.com>
2022-11-13 08:02:51 -0700

    Check queue state

--- app/workers/tumblr_worker.rb
+++ app/workers/tumblr_worker.rb
@@ -19,13 +19,17 @@ class TumblrWorker < ApplicationWorker
       oauth_token_secret: ENV['TUMBLR_ACCESS_TOKEN_SECRET']
     })
 
+    blog_info = tumblr.blog_info(tumblr_username)
+    raise blog_info.to_s if blog_info['errors'].present? || (blog_info['status'].present? && blog_info['status'] >= 400)
+    state = blog_info['blog']['queue'] > 0 ? 'queue' : 'published'
+
     opts = {
       tags: entry.tumblr_tags,
       slug: entry.slug,
       caption: entry.tumblr_caption,
       source_url: entry.permalink_url,
       format: 'markdown',
-      state: 'queue',
+      state: state,
       date: entry.published_at.to_s,
       data: entry&.photos&.map { |p| URI.open(p.url(w: 2048)).path }
     }.compact
@@ -33,7 +37,7 @@ class TumblrWorker < ApplicationWorker
     response = tumblr.photo(tumblr_username, opts)
     raise response.to_s if response['errors'].present? || (response['status'].present? && response['status'] >= 400)
 
-    if response['state'] == 'published' && response['id_string'].present?
+    if state == 'published' && response['id_string'].present?
       entry.tumblr_id = response['id_string']
       entry.save
       TumblrReblogKeyWorker.perform_async(entry.id)
