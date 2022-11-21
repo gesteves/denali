7cd0afd5607600817a1aa5a4de3a4bc9146eb094
Guillermo Esteves <g@gesteves.com>
2022-11-21 07:28:06 -0700

    Support state auto

--- app/workers/tumblr_reblog_worker.rb
+++ app/workers/tumblr_reblog_worker.rb
@@ -1,7 +1,7 @@
 class TumblrReblogWorker < ApplicationWorker
   sidekiq_options queue: 'high'
 
-  def perform(entry_id, text, state = 'published')
+  def perform(entry_id, text, state = 'auto')
     return if !Rails.env.production?
     return if ENV['TUMBLR_CONSUMER_KEY'].blank? || ENV['TUMBLR_CONSUMER_SECRET'].blank? || ENV['TUMBLR_ACCESS_TOKEN'].blank? || ENV['TUMBLR_ACCESS_TOKEN_SECRET'].blank?
 
@@ -17,6 +17,13 @@ class TumblrReblogWorker < ApplicationWorker
       oauth_token: ENV['TUMBLR_ACCESS_TOKEN'],
       oauth_token_secret: ENV['TUMBLR_ACCESS_TOKEN_SECRET']
     })
+
+    if state == 'auto'
+      blog_info = tumblr.blog_info(tumblr_username)
+      raise blog_info.to_s if blog_info['errors'].present? || (blog_info['status'].present? && blog_info['status'] >= 400)
+      # If the queue is empty, publish directly; if not, send it to the back of the queue.
+      state = blog_info['blog']['queue'] > 0 ? 'queue' : 'published'
+    end
 
     opts = {
       id: entry.tumblr_id,
