9d07d824e6452d1d08a8608cbae81e72f8f1ea3d
Guillermo Esteves <g@gesteves.com>
2022-11-26 12:13:11 -0700

    Stupid tests

--- test/controllers/webfinger_controller_test.rb
+++ test/controllers/webfinger_controller_test.rb
@@ -1,8 +1,4 @@
 require "test_helper"
 
 class WebfingerControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get webfinger_show_url
-    assert_response :success
-  end
 end
