accbff9934d5f4f626b0c4fff7f93382d0ecc508
Guillermo Esteves <g@gesteves.com>
2022-11-27 15:26:26 -0700

    Render plain

--- app/controllers/activitypub/inbox_controller.rb
+++ app/controllers/activitypub/inbox_controller.rb
@@ -2,6 +2,6 @@ class Activitypub::InboxController < ActivitypubController
   skip_before_action :verify_authenticity_token
   def index
     logger.info params
-    render text: 'OK'
+    render plain: 'OK'
   end
 end
