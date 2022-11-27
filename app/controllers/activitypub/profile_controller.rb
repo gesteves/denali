b5fff193c91932cd72cafd1045ad7d3d69566329
Guillermo Esteves <g@gesteves.com>
2022-11-27 11:54:32 -0700

    Redirect to profile page

--- app/controllers/activitypub/profile_controller.rb
+++ app/controllers/activitypub/profile_controller.rb
@@ -1,8 +1,10 @@
 class Activitypub::ProfileController < ActivitypubController
   def show
     @profile = Profile.find_by_username(params[:username])
-    if is_activitystream_request? && @profile.present?
+    if @profile.present? && is_activitystream_request?
       render
+    elsif @profile.present?
+      redirect_to profile_url(username: @profile.username)
     else
       render json: {}, status: 401
     end
