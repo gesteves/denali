9364ca7b94375fbf4cf33e6dedb7aede5f3583b5
Guillermo Esteves <g@gesteves.com>
2022-11-27 09:55:24 -0700

    Add profile page to webfinger

--- app/controllers/webfinger_controller.rb
+++ app/controllers/webfinger_controller.rb
@@ -2,13 +2,25 @@ class WebfingerController < ApplicationController
   def show
     subject = params[:resource]
     username = params[:resource]&.gsub(/^acct:@?/, '')&.split('@')&.first
+
     domain = params[:resource]&.gsub(/^acct:@?/, '')&.split('@')&.last
-    site_domain = PublicSuffix.parse(ENV['DOMAIN']).domain
+    valid_domain = begin
+      domain = PublicSuffix.parse(domain).domain
+      site_domain = PublicSuffix.parse(ENV['DOMAIN']).domain
+      domain == site_domain
+    rescue
+      false
+    end
     profile = Profile.find_by_username(username)
-    if profile.present? && domain == site_domain
+    if profile.present? && valid_domain
       response = {
         subject: subject,
         links: [
+          {
+            rel: 'http://webfinger.net/rel/profile-page',
+            type: 'text/html',
+            href: profile_url(username: profile.username)
+          },
           {
             rel: 'self',
             type: 'application/activity+json',
