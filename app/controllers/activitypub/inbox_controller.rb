c41aa83c5b99b10e8127bd7ba125642ba32d523e
Guillermo Esteves <g@gesteves.com>
2022-11-29 16:57:19 -0700

    Skip before action

--- app/controllers/activitypub/inbox_controller.rb
+++ app/controllers/activitypub/inbox_controller.rb
@@ -1,5 +1,6 @@
 class Activitypub::InboxController < ActivitypubController
   skip_before_action :verify_authenticity_token
+  skip_before_action :set_json_format
   def index
     request.headers.each { |key,value|
       logger.info "#{key}='#{value}'" if key.start_with? "HTTP"
