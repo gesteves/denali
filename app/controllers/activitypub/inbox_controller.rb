fe36f16e078c8fbbd7499c871328fa4fd6a20002
Guillermo Esteves <g@gesteves.com>
2022-11-29 16:35:03 -0700

    Logging

--- app/controllers/activitypub/inbox_controller.rb
+++ app/controllers/activitypub/inbox_controller.rb
@@ -1,6 +1,10 @@
 class Activitypub::InboxController < ActivitypubController
   skip_before_action :verify_authenticity_token
   def index
+    request.headers.each { |key,value|
+      logger.info "#{key}='#{value}'" if key.start_with? "HTTP"
+    }
+    logger.info params
     render plain: 'OK'
   end
 end
