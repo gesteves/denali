d49d025b8033ebf0251585094f2461026099ef35
Guillermo Esteves <g@gesteves.com>
2022-11-29 09:34:31 -0700

    Fix 500

--- app/controllers/activitypub/activities_controller.rb
+++ app/controllers/activitypub/activities_controller.rb
@@ -3,6 +3,6 @@ class Activitypub::ActivitiesController < ApplicationController
     @profile = Profile.find_by_username(params[:username])
     raise ActiveRecord::RecordNotFound if @profile.blank?
     @entry = @photoblog.entries.includes(photos: [:image_attachment, :image_blob]).published.by_user(@profile.user).find(params[:id])
-    raise ActiveRecord::RecordNotFound if @entry.empty?
+    raise ActiveRecord::RecordNotFound if @entry.blank?
   end
 end
