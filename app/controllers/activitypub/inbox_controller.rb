53d5b152cd9254322fa9be9633c9647acbbc5b64
Guillermo Esteves <g@gesteves.com>
2022-11-29 17:02:09 -0700

    Log signature only

--- app/controllers/activitypub/inbox_controller.rb
+++ app/controllers/activitypub/inbox_controller.rb
@@ -1,11 +1,10 @@
 class Activitypub::InboxController < ActivitypubController
   skip_before_action :verify_authenticity_token
   skip_before_action :set_json_format
+
   def index
-    request.headers.each { |key,value|
-      logger.info "#{key}='#{value}'" if key.start_with? "HTTP"
-    }
-    logger.info params
+    logger.info request.headers['signature']
+    logger.info params[:inbox]
     render plain: 'OK'
   end
 end
