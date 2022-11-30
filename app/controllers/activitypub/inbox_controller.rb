5eb31a6d57d5a28ad352864dd75d0136eb2edcb2
Guillermo Esteves <g@gesteves.com>
2022-11-29 20:37:05 -0700

    Attempt at verification

--- app/controllers/activitypub/inbox_controller.rb
+++ app/controllers/activitypub/inbox_controller.rb
@@ -3,8 +3,35 @@ class Activitypub::InboxController < ActivitypubController
   skip_before_action :set_json_format
 
   def index
-    logger.info request.headers['signature']
-    logger.info params[:inbox]
-    render plain: 'OK'
+    @user = User.find(params[:user_id])
+
+    signature_header = request.headers['signature'].split(',').map do |pair|
+      pair.split('=',2).map do |value|
+        value.gsub(/(^"|"$)/, '') # "foo" -> foo
+      end
+    end.to_h
+
+    key_id    = signature_header['keyId']
+    headers   = signature_header['headers']
+    signature = Base64.decode64(signature_header['signature'])
+
+    actor = JSON.parse(HTTParty.get(key_id, headers: { 'Accept': 'application/activity+json' }).body)
+    key   = OpenSSL::PKey::RSA.new(actor['publicKey']['publicKeyPem'])
+
+    comparison_string = headers.split(' ').map do |signed_header_name|
+      if signed_header_name == '(request-target)'
+        "(request-target): post #{request.path}"
+      elsif signed_header_name == 'host'
+        "host: #{ENV['DOMAIN']}"
+      else
+        "#{signed_header_name}: #{request.headers[signed_header_name.capitalize]}"
+      end
+    end.join("\n")
+
+    if key.verify(OpenSSL::Digest::SHA256.new, signature, comparison_string)
+      render plain: 'OK'
+    else
+      render plain: 'Unauthorized', status: 401
+    end
   end
 end
