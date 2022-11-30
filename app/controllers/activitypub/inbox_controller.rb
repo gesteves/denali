64d9a105bfb6b3fb9d94c238944e798884379543
Guillermo Esteves <g@gesteves.com>
2022-11-29 21:47:00 -0700

    Wrap in rescue

--- app/controllers/activitypub/inbox_controller.rb
+++ app/controllers/activitypub/inbox_controller.rb
@@ -4,46 +4,46 @@ class Activitypub::InboxController < ActivitypubController
 
   def index
     @user = User.find(params[:user_id])
-    logger.info params
-    logger.info request.raw_post
-
-    signature_header = request.headers['signature']&.split(',')&.map do |pair|
-      pair.split('=',2).map do |value|
-        value.gsub(/(^"|"$)/, '') # "foo" -> foo
-      end
-    end&.to_h
-
-    if signature_header.blank?
-      render plain: 'Bad request', status: 400
-      return
-    end
-
-    key_id    = signature_header['keyId']
-    headers   = signature_header['headers']
-    signature = Base64.decode64(signature_header['signature'])
-
     begin
+      logger.info request.raw_post
+      body = JSON.parse(request.raw_post)
+
+      signature_header = request.headers['signature']&.split(',')&.map do |pair|
+        pair.split('=',2).map do |value|
+          value.gsub(/(^"|"$)/, '') # "foo" -> foo
+        end
+      end&.to_h
+
+      if signature_header.blank?
+        render plain: 'Bad request', status: 400
+        return
+      end
+
+      key_id    = signature_header['keyId']
+      headers   = signature_header['headers']
+      signature = Base64.decode64(signature_header['signature'])
+
       actor = JSON.parse(HTTParty.get(key_id, headers: { 'Accept': 'application/activity+json' }).body)
       key   = OpenSSL::PKey::RSA.new(actor['publicKey']['publicKeyPem'])
+
+
+      comparison_string = headers.split(' ').map do |signed_header_name|
+        if signed_header_name == '(request-target)'
+          "(request-target): post #{request.path}"
+        elsif signed_header_name == 'host'
+          "host: #{ENV['DOMAIN']}"
+        else
+          "#{signed_header_name}: #{request.headers[signed_header_name.capitalize]}"
+        end
+      end.join("\n")
+
+      if key.verify(OpenSSL::Digest::SHA256.new, signature, comparison_string)
+        render plain: 'OK'
+      else
+        render plain: 'Unauthorized', status: 401
+      end
     rescue
       render plain: 'Bad request', status: 400
-      return
-    end
-
-    comparison_string = headers.split(' ').map do |signed_header_name|
-      if signed_header_name == '(request-target)'
-        "(request-target): post #{request.path}"
-      elsif signed_header_name == 'host'
-        "host: #{ENV['DOMAIN']}"
-      else
-        "#{signed_header_name}: #{request.headers[signed_header_name.capitalize]}"
-      end
-    end.join("\n")
-
-    if key.verify(OpenSSL::Digest::SHA256.new, signature, comparison_string)
-      render plain: 'OK'
-    else
-      render plain: 'Unauthorized', status: 401
     end
   end
 end
