b4124b926dd4a6745fb056b7027d88a31e9733c5
Guillermo Esteves <g@gesteves.com>
2022-12-01 09:16:58 -0700

    Add a barebones mastodon lib

---
+++ app/lib/mastodon.rb
@@ -0,0 +1,51 @@
+require 'open-uri'
+require 'json'
+class Mastodon
+  def initialize(base_url:, bearer_token:)
+    @bearer_token = bearer_token
+    @base_url = base_url
+  end
+
+  def create_status(text:, media_ids: nil)
+    endpoint = "#{@base_url}/api/v1/statuses"
+
+    body = {
+      status: HTMLEntities.new.decode(text),
+      media_ids: media_ids
+    }.compact
+
+    headers = {
+      'Authorization': "Bearer #{@bearer_token}"
+    }
+
+    response = HTTParty.post(endpoint, body: body, headers: headers)
+
+    if response.code == 200
+      JSON.parse(response.body)
+    else
+      raise response.body
+    end
+  end
+
+  def upload_media(url:, alt_text: nil, focal_point: nil)
+  endpoint = "#{@base_url}/api/v2/media"
+
+    body = {
+      file: URI.open(url),
+      description: HTMLEntities.new.decode(alt_text),
+      focus: focal_point
+    }.compact
+
+    headers = {
+      'Authorization': "Bearer #{@bearer_token}"
+    }
+
+    response = HTTParty.post(endpoint, body: body, headers: headers)
+
+    if response.code == 200
+      JSON.parse(response.body)
+    else
+      raise response.body
+    end
+  end
+end
--- app/workers/mastodon_worker.rb
+++ app/workers/mastodon_worker.rb
@@ -8,8 +8,8 @@ class MastodonWorker < ApplicationWorker
     return if !entry.is_photo?
     raise UnprocessedPhotoError unless entry.photos_have_dimensions?
 
-    mastodon = Mastodon::REST::Client.new(base_url: ENV['MASTODON_BASE_URL'], bearer_token: ENV['MASTODON_ACCESS_TOKEN'])
-    media_ids = entry.photos.map { |p| mastodon.upload_media(URI.open(p.mastodon_url).path, description: p.alt_text, focus: p.mastodon_focal_point.join(',')).id }
+    mastodon = Mastodon.new(base_url: ENV['MASTODON_BASE_URL'], bearer_token: ENV['MASTODON_ACCESS_TOKEN'])
+    media_ids = entry.photos.map { |p| mastodon.upload_media(p.mastodon_url, alt_text: p.alt_text, focal_point: p.mastodon_focal_point.join(',')).id }
     mastodon.create_status(text, media_ids: media_ids)
   end
 
