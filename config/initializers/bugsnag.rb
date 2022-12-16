e77c3e96bc1871466470aedf0f48c606eec57b2b
Guillermo Esteves <g@gesteves.com>
2022-12-16 14:30:02 -0700

    Ignore errors

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,5 +1,11 @@
 Bugsnag.configure do |config|
   config.api_key = ENV['BUGSNAG_API_KEY']
   config.app_version = ENV['HEROKU_RELEASE_VERSION'] || ENV['RENDER_GIT_COMMIT'] || ENV['FLY_ALLOC_ID']
-  config.discard_classes += ['ActiveRecord::RecordNotFound', 'UnprocessedPhotoError', 'TumblrPostDelayedPublishError', 'Aws::CloudFront::Errors::TooManyInvalidationsInProgress']
+  config.discard_classes += %w{
+    ActiveRecord::RecordNotFound
+    Aws::CloudFront::Errors::ServiceUnavailable
+    Aws::CloudFront::Errors::TooManyInvalidationsInProgress
+    TumblrPostDelayedPublishError
+    UnprocessedPhotoError
+  }
 end
