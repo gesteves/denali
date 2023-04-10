e8b676ee540a5ef116de155d0048e9adb4ea73f7
Guillermo Esteves <g@gesteves.com>
2023-04-10 09:11:09 -0600

    Tweak concern

--- app/models/concerns/thumborizable.rb
+++ app/models/concerns/thumborizable.rb
@@ -1,9 +1,9 @@
 module Thumborizable
   extend ActiveSupport::Concern
 
-  def thumbor_url(opts = {})
-    return if self&.image&.key.blank?
-    opts.merge!(image: "#{ENV['S3_BUCKET']}/#{self.image.key}")
+  def thumbor_url(key, opts = {})
+    return if key.blank?
+    opts.merge!(image: "#{ENV['S3_BUCKET']}/#{key}")
     path = Thumbor.generate(opts)
     "https://#{ENV['THUMBOR_DOMAIN']}/thumbor#{path}"
   end
