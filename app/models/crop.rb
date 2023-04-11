50c191c6bb1380ce0e9d36dcc139ea2e63b43785
Guillermo Esteves <g@gesteves.com>
2023-04-10 19:41:53 -0600

    Clean up code

--- app/models/crop.rb
+++ app/models/crop.rb
@@ -6,39 +6,17 @@ class Crop < ApplicationRecord
   validates :height, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 1}
   validates :aspect_ratio, presence: true, uniqueness: { scope: :photo_id }
 
-  def computed_width
-    (self.width * photo.width).round
-  end
-
-  def computed_height
-    (self.height * photo.height).round
-  end
-
-  def computed_x
-    (self.x * photo.width).round
-  end
-
-  def computed_y
-    (self.y * photo.height).round
-  end
-
-  def left
-    self.computed_x
-  end
-
-  def right
-    self.left + self.computed_width
-  end
-
-  def top
-    self.computed_y
-  end
-
-  def bottom
-    self.computed_y + self.computed_height
-  end
-
   def to_rect
-    [self.left, self.top, self.right, self.bottom]
+    # Retrieve the photo's dimensions
+    photo_width = photo.width
+    photo_height = photo.height
+
+    # Calculate the absolute pixel coordinates
+    left = x * photo_width
+    top = y * photo_height
+    right = left + (width * photo_width)
+    bottom = top + (height * photo_height)
+
+    [left, top, right, bottom].map(&:round)
   end
 end
