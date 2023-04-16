eef302e0f87ca1d5a8a0f5bfdf00f8a904e32977
Guillermo Esteves <g@gesteves.com>
2023-04-16 12:29:03 -0600

    Specify width

--- app/workers/blurhash_worker.rb
+++ app/workers/blurhash_worker.rb
@@ -5,7 +5,7 @@ class BlurhashWorker < ApplicationWorker
     photo = Photo.find(photo_id)
     raise UnprocessedPhotoError unless photo.has_dimensions?
 
-    image = MiniMagick::Image.open(photo.url)
+    image = MiniMagick::Image.open(photo.url(width: 1200))
 
     if blurhash = Blurhash.encode(image.width, image.height, image.get_pixels.flatten).presence
       photo.blurhash = blurhash
