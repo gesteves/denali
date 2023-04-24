f8ad68bf4461826164d736ff53c05588a0daad90
Guillermo Esteves <g@gesteves.com>
2023-04-24 15:06:28 -0600

    Use different method

--- app/models/concerns/thumborizable.rb
+++ app/models/concerns/thumborizable.rb
@@ -23,6 +23,7 @@ module Thumborizable
     filters = []
     filters << "fill(#{opts[:fill]},true)" if opts[:fill].present?
     filters << "quality(#{opts[:quality]})" if opts[:quality].present?
+    filters << "grayscale()" if opts[:grayscale].present?
     filters << "format(#{opts[:format]})" if opts[:format].present? && VALID_FORMATS.include?(opts[:format])
     filters
   end
--- app/workers/color_detection_worker.rb
+++ app/workers/color_detection_worker.rb
@@ -4,7 +4,7 @@ class ColorDetectionWorker < ApplicationWorker
     photo = Photo.find(photo_id)
     raise UnprocessedPhotoError unless photo.has_dimensions?
 
-    is_bw = is_black_and_white?(photo.url(width: 300))
+    is_bw = is_black_and_white?(photo)
     photo.black_and_white = is_bw
     photo.color = !is_bw
     photo.save
@@ -12,16 +12,21 @@ class ColorDetectionWorker < ApplicationWorker
 
   private
 
-  def is_black_and_white?(image_url)
-    image = MiniMagick::Image.open(image_url)
-    histogram = image.run_command('convert', image.path, '-format', '%c', '-colors', '16', 'histogram:info:-')
-    histogram.lines.each do |line|
-      if line =~ /^\s*\d*:.*\s([a-fA-F0-9]{6})\s/
-        color = $1
-        red, green, blue = color[0..1].to_i(16), color[2..3].to_i(16), color[4..5].to_i(16)
-        return false if red != green || green != blue
-      end
-    end
-    true
+  def is_black_and_white?(photo)
+    # Load the images with MiniMagick
+    original_image = MiniMagick::Image.open(photo.url(width: 300))
+    grayscaled_image = MiniMagick::Image.open(photo.url(width: 300, grayscale: true))
+  
+    # Calculate the mean error per pixel between the two images
+    compare_result = original_image.compare(grayscaled_image, "mae")
+    mean_error_per_pixel = compare_result[1].to_f
+  
+    # Get the threshold from the environment variable or use a default value of 5
+    threshold = ENV.fetch('MAE_THRESHOLD', 5).to_f
+  
+    # Return true if the mean error per pixel is below the threshold
+    mean_error_per_pixel <= threshold
+  rescue StandardError => e
+    false
   end
 end
