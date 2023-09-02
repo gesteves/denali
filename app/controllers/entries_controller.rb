2ee60eccabc4f6a05ade603b8c02dfd36165c7c4
Guillermo Esteves <g@gesteves.com>
2023-09-02 13:13:38 -0600

    Expand range

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -125,7 +125,7 @@ class EntriesController < ApplicationController
   end
 
   def random
-    entry = Entry.find(Entry.published.where('published_at >= ?', 3.years.ago).pluck(:id).sample)
+    entry = Entry.find(Entry.published.where('published_at >= ?', 4.years.ago).pluck(:id).sample)
     response.headers['Cache-Control'] = "s-maxage=#{ENV['RANDOM_CACHE_TTL']}, max-age=0, public"
     redirect_to entry.permalink_url, status: 302
   end
--- app/workers/random_share_worker.rb
+++ app/workers/random_share_worker.rb
@@ -3,7 +3,7 @@ class RandomShareWorker < ApplicationWorker
     logger.info "[Queue] Attempting to share a random entry."
     photoblog = Blog.first
     return if photoblog.entries.published.first.published_at >= 1.day.ago
-    entry = Entry.find(Entry.published.where('published_at >= ?', 3.years.ago).pluck(:id).sample)
+    entry = Entry.find(Entry.published.where('published_at >= ?', 4.years.ago).pluck(:id).sample)
     MastodonWorker.perform_async(entry.id, entry.mastodon_caption) if entry.post_to_mastodon
     BlueskyWorker.perform_async(entry.id, entry.bluesky_caption) if entry.post_to_bluesky
   end
