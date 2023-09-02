100b3a7b03dc71fbba570436241cf1b9662e0a64
Guillermo Esteves <g@gesteves.com>
2023-09-02 12:51:49 -0600

    Share a random photo every day

---
+++ app/workers/random_share_worker.rb
@@ -0,0 +1,10 @@
+class RandomShareWorker < ApplicationWorker
+  def perform
+    logger.info "[Queue] Attempting to share a random entry."
+    photoblog = Blog.first
+    return if photoblog.entries.published.first.published_at >= 1.day.ago
+    entry = Entry.find(Entry.published.where('published_at >= ?', 3.years.ago).pluck(:id).sample)
+    MastodonWorker.perform_async(entry.id, entry.mastodon_caption) if entry.post_to_mastodon
+    BlueskyWorker.perform_async(entry.id, entry.bluesky_caption) if entry.post_to_bluesky
+  end
+end
