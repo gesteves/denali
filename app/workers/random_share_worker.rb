6b04e8ea4d82b04138bfe6d56b7d0e95453014e5
Guillermo Esteves <g@gesteves.com>
2023-09-02 13:48:05 -0600

    Have a way to pause the random shares

--- app/workers/random_share_worker.rb
+++ app/workers/random_share_worker.rb
@@ -1,8 +1,9 @@
 class RandomShareWorker < ApplicationWorker
   def perform
+    return if ENV['SHARE_RANDOM_PHOTOS'].blank?
     logger.info "[Queue] Attempting to share a random entry."
     photoblog = Blog.first
-    return if photoblog.entries.published.first.published_at >= 1.day.ago
+    return if photoblog.entries.published.first.published_at >= 2.days.ago || Entry.queued.count > 0
     entry = Entry.find(Entry.published.where('published_at >= ?', 4.years.ago).pluck(:id).sample)
     MastodonWorker.perform_async(entry.id, entry.mastodon_caption) if entry.post_to_mastodon
     BlueskyWorker.perform_async(entry.id, entry.bluesky_caption) if entry.post_to_bluesky
