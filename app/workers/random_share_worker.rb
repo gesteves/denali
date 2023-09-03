c871d6e51ccb0bc99e6b6270dc427232cd231c77
Guillermo Esteves <g@gesteves.com>
2023-09-03 06:46:33 -0600

    Randomize more

--- app/workers/random_share_worker.rb
+++ app/workers/random_share_worker.rb
@@ -6,6 +6,7 @@ class RandomShareWorker < ApplicationWorker
     return if photoblog.entries.published.first.published_at >= 2.days.ago || Entry.queued.count > 0
     entry = Entry.find(Entry.published.where('published_at >= ?', 4.years.ago).pluck(:id).sample)
     MastodonWorker.perform_async(entry.id, entry.mastodon_caption) if entry.post_to_mastodon
+    entry = Entry.find(Entry.published.where('published_at >= ?', 4.years.ago).pluck(:id).sample)
     BlueskyWorker.perform_async(entry.id, entry.bluesky_caption) if entry.post_to_bluesky
   end
 end
