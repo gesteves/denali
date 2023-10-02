2554439a3e6e78caee477a8a88234a537656b5e8
Guillermo Esteves <g@gesteves.com>
2023-10-01 20:27:20 -0600

    Don't share if an entry was published that day

--- app/workers/random_mastodon_worker.rb
+++ app/workers/random_mastodon_worker.rb
@@ -3,6 +3,10 @@ class RandomMastodonWorker < ApplicationWorker
     return if ENV['SHARE_RANDOM_PHOTOS'].blank?
     logger.info "[Queue] Attempting to share a random entry on Bluesky."
     photoblog = Blog.first
+    
+    current_time = Time.current.in_time_zone(photoblog.time_zone)
+    return if current_time.to_date == photoblog.entries.published.first.published_at.in_time_zone(photoblog.time_zone).to_date
+
     entry = Entry.find(photoblog.entries.published.photo_entries.tagged_with(tag, any: true).where('published_at >= ?', 4.years.ago).pluck(:id).sample)
     MastodonWorker.perform_async(entry.id, entry.mastodon_caption) if entry.post_to_mastodon
   end
--- app/workers/random_tumblr_worker.rb
+++ app/workers/random_tumblr_worker.rb
@@ -4,7 +4,10 @@ class RandomTumblrWorker < ApplicationWorker
     return if ENV['SHARE_RANDOM_PHOTOS'].blank?
     logger.info "[Queue] Attempting to reblog a random entry on Tumblr."
     photoblog = Blog.first
-    return if photoblog.entries.published.first.published_at >= 2.days.ago || Entry.queued.count > 0
+    
+    current_time = Time.current.in_time_zone(photoblog.time_zone)
+    return if current_time.to_date == photoblog.entries.published.first.published_at.in_time_zone(photoblog.time_zone).to_date
+
     entry = Entry.find(Entry.posted_on_tumblr.where('published_at >= ?', 4.years.ago).pluck(:id).sample)
     TumblrReblogWorker.perform_async(entry.id)
   end
