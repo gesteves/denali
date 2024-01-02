7bec81166b34ae38185d81dbc5b633c0651c6649
Guillermo Esteves <g@gesteves.com>
2024-01-02 13:43:12 -0700

    Tweak eligibility

--- app/workers/random_share_worker.rb
+++ app/workers/random_share_worker.rb
@@ -7,16 +7,18 @@ class RandomShareWorker < ApplicationWorker
     current_time = Time.current.in_time_zone(photoblog.time_zone)
     return if current_time.to_date == photoblog.entries.published.first.published_at.in_time_zone(photoblog.time_zone).to_date
 
+    entry = find_eligible_entry(tag, platform)
+    return if entry.blank?
+
     case platform
     when 'Bluesky'
-      entry = find_eligible_entry(tag, 'Bluesky')
-      BlueskyWorker.perform_async(entry.id, entry.bluesky_caption) if entry.present?
+      BlueskyWorker.perform_async(entry.id, entry.bluesky_caption)
     when 'Mastodon'
-      entry = find_eligible_entry(tag, 'Mastodon')
-      MastodonWorker.perform_async(entry.id, entry.mastodon_caption) if entry.present?
+      MastodonWorker.perform_async(entry.id, entry.mastodon_caption)
+    when 'Instagram'
+      InstagramWorker.perform_async(entry.id, entry.instagram_caption)
     when 'Tumblr'
-      entry = find_eligible_entry(tag, 'Tumblr')
-      TumblrReblogWorker.perform_async(entry.id) if entry.present?
+      TumblrReblogWorker.perform_async(entry.id)
     end
   end
 
@@ -25,7 +27,16 @@ class RandomShareWorker < ApplicationWorker
   def find_eligible_entry(tag, platform)
     photoblog = Blog.first
 
-    eligible_entry_ids = photoblog.entries.posted_on_tumblr.photo_entries.tagged_with(tag).pluck(:id)
+    eligible_entry_ids = case platform
+    when 'Bluesky'
+      photoblog.entries.published.tagged_with(tag).where(post_to_bluesky: true).pluck(:id)
+    when 'Mastodon'
+      photoblog.entries.published.tagged_with(tag).where(post_to_mastodon: true).pluck(:id)
+    when 'Instagram'
+      photoblog.entries.published.tagged_with(tag).where(post_to_instagram: true).pluck(:id)
+    when 'Tumblr'
+      photoblog.entries.posted_on_tumblr.tagged_with(tag).pluck(:id)
+    end
 
     recently_shared_ids = $redis.lrange("recently_shared:#{platform.downcase}", 0, -1)
     eligible_entry_ids -= recently_shared_ids.map(&:to_i)
