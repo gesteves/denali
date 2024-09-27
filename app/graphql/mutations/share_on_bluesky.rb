650373671a965d4c31acaaf30483996be26d08f1
Guillermo Esteves <g@gesteves.com>
2024-09-27 09:15:03 -0600

    Add input

--- app/graphql/mutations/share_on_bluesky.rb
+++ app/graphql/mutations/share_on_bluesky.rb
@@ -1,14 +1,22 @@
 module Mutations
   class ShareOnBluesky < BaseMutation
-    argument :url, String, required: true
+    
+    class ShareOnBlueskyInput < Types::BaseInputObject
+      argument :url, String, required: true
+    end
 
     field :entry, Types::EntryType, null: false
     field :errors, [String], null: false
 
-    def resolve(url:)
+    argument :input, ShareOnBlueskyInput, required: true
+
+    def resolve(input:)
+      url = input[:url]
+
       begin
         entry = Entry.find_by_url(url: url)
         BlueskyWorker.perform_async(entry.id, entry.bluesky_caption)
+
         {
           entry: entry,
           errors: []
