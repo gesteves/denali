95183dc8acbc0855dc5e8e9edcb32323e987ba58
Guillermo Esteves <g@gesteves.com>
2025-01-04 14:16:07 -0700

    Get political results

--- app/workers/photo_geocode_worker.rb
+++ app/workers/photo_geocode_worker.rb
@@ -4,7 +4,7 @@ class PhotoGeocodeWorker < ApplicationWorker
     photo = Photo.find(photo_id)
     return if ENV['GOOGLE_API_KEY'].blank? || !photo.has_location?
     raise UnprocessedPhotoError unless photo.has_dimensions?
-    url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=#{photo.latitude},#{photo.longitude}&key=#{ENV['GOOGLE_API_KEY']}"
+    url = "https://maps.googleapis.com/maps/api/geocode/json?result_type=political&latlng=#{photo.latitude},#{photo.longitude}&key=#{ENV['GOOGLE_API_KEY']}"
     response = JSON.parse(HTTParty.get(url).body)
     if response['status'] != 'OK'
       raise "Geocode request failed: #{response.to_s}"
