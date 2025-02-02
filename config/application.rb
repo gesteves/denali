818fbb5dc458f6391185eda4567d615f02a3bc33
Guillermo Esteves <g@gesteves.com>
2025-02-02 10:54:11 -0700

    Verify that the bulma folder is there

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,5 @@ module Denali
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.assets.paths << Rails.root.join('node_modules')
   end
 end
