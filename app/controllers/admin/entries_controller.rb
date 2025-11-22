5a688d40199d5ee29e939e07bc6a65172089f0bd
Guillermo Esteves <g@gesteves.com>
2025-11-22 15:17:19 -0700

    Add ordering to entries for sharing on Bluesky, Mastodon, Threads, and Instagram

--- app/controllers/admin/entries_controller.rb
+++ app/controllers/admin/entries_controller.rb
@@ -76,6 +76,7 @@ class Admin::EntriesController < AdminController
     base_query = @photoblog.entries.published
       .where(post_to_bluesky: true)
       .where("last_shared_on_bluesky_at IS NULL OR last_shared_on_bluesky_at < ?", 1.year.ago)
+      .order(Arel.sql("CASE WHEN last_shared_on_bluesky_at IS NULL THEN 0 ELSE 1 END, CASE WHEN last_shared_on_bluesky_at IS NULL THEN published_at ELSE last_shared_on_bluesky_at END ASC"))
     @entries = base_query.includes(:blog, photos: [:image_attachment, :image_blob], taggings: :tag).page(@page)
     @page_title = 'Shareable on Bluesky'
     respond_to do |format|
@@ -89,6 +90,7 @@ class Admin::EntriesController < AdminController
     base_query = @photoblog.entries.published
       .where(post_to_mastodon: true)
       .where("last_shared_on_mastodon_at IS NULL OR last_shared_on_mastodon_at < ?", 1.year.ago)
+      .order(Arel.sql("CASE WHEN last_shared_on_mastodon_at IS NULL THEN 0 ELSE 1 END, CASE WHEN last_shared_on_mastodon_at IS NULL THEN published_at ELSE last_shared_on_mastodon_at END ASC"))
     @entries = base_query.includes(:blog, photos: [:image_attachment, :image_blob], taggings: :tag).page(@page)
     @page_title = 'Shareable on Mastodon'
     respond_to do |format|
@@ -102,6 +104,7 @@ class Admin::EntriesController < AdminController
     base_query = @photoblog.entries.published
       .where(post_to_threads: true)
       .where("last_shared_on_threads_at IS NULL OR last_shared_on_threads_at < ?", 1.year.ago)
+      .order(Arel.sql("CASE WHEN last_shared_on_threads_at IS NULL THEN 0 ELSE 1 END, CASE WHEN last_shared_on_threads_at IS NULL THEN published_at ELSE last_shared_on_threads_at END ASC"))
     @entries = base_query.includes(:blog, photos: [:image_attachment, :image_blob], taggings: :tag).page(@page)
     @page_title = 'Shareable on Threads'
     respond_to do |format|
@@ -115,6 +118,7 @@ class Admin::EntriesController < AdminController
     base_query = @photoblog.entries.published
       .where(post_to_instagram: true)
       .where("last_shared_on_instagram_at IS NULL OR last_shared_on_instagram_at < ?", 1.year.ago)
+      .order(Arel.sql("CASE WHEN last_shared_on_instagram_at IS NULL THEN 0 ELSE 1 END, CASE WHEN last_shared_on_instagram_at IS NULL THEN published_at ELSE last_shared_on_instagram_at END ASC"))
     @entries = base_query.includes(:blog, photos: [:image_attachment, :image_blob], taggings: :tag).page(@page)
     @page_title = 'Shareable on Instagram'
     respond_to do |format|
