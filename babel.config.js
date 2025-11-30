b74eb721d3002270b4a2821532d658e6c2278937
Guillermo Esteves <g@gesteves.com>
2025-11-30 08:26:50 -0700

    Silence babel warnings

--- babel.config.js
+++ babel.config.js
@@ -48,6 +48,18 @@ module.exports = function(api) {
         }
       ],
       [
+        '@babel/plugin-transform-private-methods',
+        {
+          loose: true
+        }
+      ],
+      [
+        '@babel/plugin-transform-private-property-in-object',
+        {
+          loose: true
+        }
+      ],
+      [
         '@babel/plugin-proposal-object-rest-spread',
         {
           useBuiltIns: true
