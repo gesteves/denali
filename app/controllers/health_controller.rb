d7892ffc05692629831e4b780412e238f4d31e4b
Guillermo Esteves <g@gesteves.com>
2025-12-04 08:04:25 -0700

    Update healthcheck

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -3,8 +3,15 @@ class HealthController < ApplicationController
   skip_before_action :domain_redirect
 
   def show
-    render plain: "OK", status: 200
+    render plain: "OK", status: :ok
   rescue
-    render plain: "Not OK", status: 500
+    render plain: "Not OK", status: :service_unavailable
+  end
+
+  def ready
+    ActiveRecord::Base.connection.execute("SELECT 1")
+    render plain: "OK", status: :ok
+  rescue StandardError
+    render plain: "Not OK", status: :service_unavailable
   end
 end
