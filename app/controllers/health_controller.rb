5f7514e2658aa3e04bc2a655e0a6c340d1b7897c
Guillermo Esteves <g@gesteves.com>
2025-12-08 06:47:51 -0700

    Update health endpoint

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -1,6 +1,6 @@
 class HealthController < ApplicationController
   before_action :no_cache
-  skip_before_action :domain_redirect
+  skip_before_action :domain_redirect, :get_photoblog
 
   def show
     # Check database connection
