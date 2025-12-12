48ae002a726631aeb9bce0a5083393408077afe8
Guillermo Esteves <g@gesteves.com>
2025-12-12 07:09:30 -0700

    Move skip_before_action

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -1,8 +1,8 @@
 class HealthController < ApplicationController
   before_action :no_cache
   skip_before_action :verify_authenticity_token
-  skip_before_action :domain_redirect
   skip_before_action :get_photoblog
+  skip_before_action :domain_redirect
 
   def show
     # Check database connection
