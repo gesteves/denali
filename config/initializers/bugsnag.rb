889eba94cddc17c53168c60848ef6cf3ce2ef7fd
Guillermo Esteves <g@gesteves.com>
2025-12-12 19:06:01 -0700

    Only log errors in prod

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,5 +1,6 @@
 Bugsnag.configure do |config|
   config.api_key = ENV['BUGSNAG_API_KEY']
+  config.enabled_release_stages = %w[production]
   config.discard_classes += %w{
     ActiveRecord::RecordNotFound
     Aws::CloudFront::Errors::ServiceUnavailable
