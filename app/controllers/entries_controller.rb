ae3258b5c05819ec72c605c3653a7c31bd4e50e4
Guillermo Esteves <g@gesteves.com>
2025-12-13 10:42:22 -0700

    Remove env var

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -143,7 +143,7 @@ class EntriesController < ApplicationController
 
   def random
     entry = Entry.find(Entry.published.where('published_at >= ?', 4.years.ago).pluck(:id).sample)
-    response.headers['Cache-Control'] = "s-maxage=#{ENV['RANDOM_CACHE_TTL']}, max-age=0, public"
+    response.headers['Cache-Control'] = "s-maxage=1, max-age=0, public"
     redirect_to entry.permalink_url, status: 302
   end
 
