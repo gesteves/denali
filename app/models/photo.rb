4edc9b3f19624562b368463e0737ab774b491072
Guillermo Esteves <g@gesteves.com>
2025-12-15 19:46:45 -0700

    Enqueue job

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -42,12 +42,7 @@ class Photo < ApplicationRecord
 
   def update_entry_caption_validity
     return if self.entry.nil? || self.entry.destroyed?
-    self.entry.update_columns(
-      valid_bluesky_caption: Bluesky.valid_post_length?(entry.bluesky_caption),
-      valid_mastodon_caption: entry.mastodon_caption.length <= 500,
-      valid_instagram_caption: entry.instagram_caption.length <= 2200,
-      valid_threads_caption: entry.threads_caption.length <= 500
-    )
+    CaptionValidityWorker.perform_async(self.entry.id)
   end
 
   def self.oldest
