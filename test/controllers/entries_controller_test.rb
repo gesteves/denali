dc4cfed7ce24f05d5994b0da1bd3e56a2a5e0d29
Guillermo Esteves <g@gesteves.com>
2025-12-17 18:22:03 -0700

    Update test

--- test/controllers/entries_controller_test.rb
+++ test/controllers/entries_controller_test.rb
@@ -47,7 +47,7 @@ class EntriesControllerTest < ActionController::TestCase
     assert_template :show
     assert_select '.entry', 1
     assert_select '.entry__photo', 1
-    assert_select '.entry__figure', 1
+    assert_select '.entry__photos', 1
     assert_select '.entry__headline', 1
   end
 
