83280e1871bfbbe8b715e7ac5be208c93859ecc3
Guillermo Esteves <g@gesteves.com>
2025-12-20 22:02:38 -0700

    Set max age

--- app/controllers/robots_controller.rb
+++ app/controllers/robots_controller.rb
@@ -1,6 +1,6 @@
 class RobotsController < ApplicationController
+  before_action :set_max_age
   def show
-    expires_in 24.hours, public: true
     @ai_agents = Rails.cache.fetch("dark-visitors", expires_in: 1.day) do
       dark_visitors = DarkVisitors.new(access_token: ENV['DARK_VISITORS_ACCESS_TOKEN'])
       dark_visitors.robots_txt.presence
