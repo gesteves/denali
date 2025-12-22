38824032005cb4c7d7b3ef509437d8c5b7a864b5
Guillermo Esteves <g@gesteves.com>
2025-12-21 20:36:22 -0700

    Cache related

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -150,11 +150,6 @@ class EntriesController < ApplicationController
       format.html {
         redirect_to @entry.permalink_url, status: 301 if request.path != @entry.permalink_path
         @page_title = "#{@entry.plain_title} – #{@photoblog.name}"
-        if @photoblog.show_related_entries?
-          @related_entries = @entry.related
-          @related_srcset = PHOTOS[:entry_list_square][:srcset]
-          @related_sizes = PHOTOS[:entry_list_square][:sizes].join(', ')
-        end
       }
       format.all { redirect_to(@entry.permalink_url, status: 301) }
     end
