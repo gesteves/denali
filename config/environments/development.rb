bd71af9e18e4a1cb51f4519c9b845e9e8520852d
Guillermo Esteves <g@gesteves.com>
2026-01-17 10:29:52 -0700

    Put activejob on sidekiq

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,6 +54,9 @@ Rails.application.configure do
   # Highlight code that enqueued background job in logs.
   config.active_job.verbose_enqueue_logs = true
 
+  # Use Sidekiq for Active Job (matches production)
+  config.active_job.queue_adapter = :sidekiq
+
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
