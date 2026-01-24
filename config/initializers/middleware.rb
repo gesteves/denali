08bcb0a465927180a355db0d1533f1622569f773
Guillermo Esteves <g@gesteves.com>
2026-01-24 12:05:58 -0700

    Fix middleware

--- config/initializers/middleware.rb
+++ config/initializers/middleware.rb
@@ -1,2 +1,4 @@
-Rails.application.config.middleware.insert_before ActionDispatch::Static, Rack::Deflater
-Rails.application.config.middleware.insert_before ActionDispatch::Static, Rack::Brotli
+if Rails.application.config.public_file_server.enabled
+  Rails.application.config.middleware.insert_before ActionDispatch::Static, Rack::Deflater
+  Rails.application.config.middleware.insert_before ActionDispatch::Static, Rack::Brotli
+end
